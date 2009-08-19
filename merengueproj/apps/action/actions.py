2cac0ab78cc98c548f3642314d3f476ddf32122b
msaelices <msaelices@yaco.es>
2009-08-19 00:14:32 +0000

    A functional configuration example, without saving smart selectors (only saving as json field). See #34.
    git-svn-id: https://svn.merengueproject.org/trunk@77 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/apps/action/actions.py
+++ merengueproj/apps/action/actions.py
@@ -1,6 +1,7 @@
 from action.models import RegisteredAction
-#from registry import params
+from registry import params
 from registry.items import RegistryItem
+from django.utils.translation import ugettext_lazy as _
 
 
 class BaseAction(RegistryItem):
@@ -18,7 +19,19 @@ class SiteAction(BaseAction):
 
 
 class UserAction(BaseAction):
-    pass
+    # note: this config params are only for debugging purposes. remove it after was useful
+    config_params = [
+        params.Single(name='username', label=_('username'), default='pepe'),
+        params.List(name='friends', label=_('friends'), default=['antonio', 'juan'],
+                    choices=[('antonio', 'Antonio'),
+                             ('paco', 'Paco'),
+                             ('rosa', 'Rosa')]),
+        params.Single(name='season', label=_('season'),
+                      choices=[('spring', _('Spring')),
+                               ('summer', _('Summer')),
+                               ('autumn', _('Autumn')),
+                               ('winter', _('Winter'))]),
+    ]
 
 
 class ContentAction(BaseAction):
--- merengueproj/apps/registry/widgets.py
+++ merengueproj/apps/registry/widgets.py
@@ -10,8 +10,10 @@ class ConfigWidget(JSONWidget):
         self.config = None # to be filled in registry model admin
 
     def render(self, name, value, attrs=None):
+        debug_widget = super(ConfigWidget, self).render(name, value, attrs)
         return render_to_string('registry/configwidget.html',
                                 {'config_params': self.config,
                                  'name': name,
                                  'value': value,
+                                 'debug_widget': debug_widget,
                                  'attrs': attrs})
