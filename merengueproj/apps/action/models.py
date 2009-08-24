8b1946962ec4d183fd3eba6a5d4b429fcbed5c7f
msaelices <msaelices@yaco.es>
2009-08-24 14:09:43 +0000

    Better with a __unicode__ method. See #1.
    git-svn-id: https://svn.merengueproject.org/trunk@113 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/apps/action/models.py
+++ merengueproj/apps/action/models.py
@@ -5,3 +5,6 @@ from registry.models import RegisteredItem
 
 class RegisteredAction(RegisteredItem):
     name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.name
