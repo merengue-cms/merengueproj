c63456898aab2d43e9777791096ad70025fefdcf
msaelices <msaelices@yaco.es>
2009-08-25 14:12:37 +0000

    After one hour, I discovered that's the problem. Fixes #49.
    git-svn-id: https://svn.merengueproject.org/trunk@136 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/settings.py
+++ merengueproj/merengue/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.transaction.TransactionMiddleware',
     'pagination.middleware.PaginationMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'cmsutils.middleware.AutomatizedTestingMiddleware',
