a7fa30e9652eaaf77dd0d090f6c5cb58df925871
msaelices <msaelices@yaco.es>
2009-08-31 06:51:43 +0000

    Facebook plugin for merengue. Fixes #74.
    git-svn-id: https://svn.merengueproject.org/trunk@195 5c12d508-d76f-0410-91c7-e3e856e0eb0f

---
+++ merengueproj/plugins/facebook/actions.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponseRedirect
+from django.utils.http import urlquote
+from django.utils.translation import ugettext_lazy as _
+
+from merengue.action.actions import ContentAction
+
+
+class FacebookLink(ContentAction):
+    name = 'facebooklink'
+    verbose_name = _('Link at Facebook')
+
+    @classmethod
+    def get_response(cls, request, content):
+        request_url = 'http://%s%s' % (request.get_host(), request.get_full_path())
+        return HttpResponseRedirect('http://www.facebook.com/share.php?u=%s' % urlquote(request_url))
---
+++ merengueproj/plugins/facebook/config.py
@@ -0,0 +1,16 @@
+from merengue.plug import Plugin
+
+from plugins.facebook.actions import FacebookLink
+
+
+class PluginConfig(Plugin):
+    name = 'Facebook'
+    description = 'Facebook integration plugin'
+    version = '0.0.1a'
+    url_prefixes = (
+        #('news', 'plugins.facebook.urls'),
+    )
+
+    @classmethod
+    def get_actions(cls):
+        return [FacebookLink]
