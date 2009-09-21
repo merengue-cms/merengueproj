cbddb3b03a462360d342eebec57d351b45d3e78d
msaelices <msaelices@yaco.es>
2009-09-21 09:59:34 +0000

    Removing deprecated metaclass. See #109.
    git-svn-id: https://svn.merengueproject.org/trunk@310 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/news/models.py
+++ merengueproj/plugins/news/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.db.models import permalink
 from django.utils.translation import ugettext_lazy as _
 
-from transmeta import TransMeta
-
 from merengue.base.models import BaseContent, BaseCategory
 from plugins.news.managers import NewsItemManager
 
@@ -20,7 +18,6 @@ class NewsCategory(BaseCategory):
 
 
 class NewsItem(BaseContent):
-    __metaclass__ = TransMeta
 
     publish_date = models.DateTimeField(blank=True, null=True, db_index=True)
     expire_date = models.DateTimeField(blank=True, null=True, db_index=True)
