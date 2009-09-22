dece83a7caa687a4b5cc6266281d10d1bfbd644e
Emilio A. Sanchez Lopez <esanchez@cymoril>
2009-09-22 07:29:36 +0000

    Migration for new transmeta fields in collab app. See #96 no-pep8, no-pyflakes
    git-svn-id: https://svn.merengueproject.org/trunk@341 5c12d508-d76f-0410-91c7-e3e856e0eb0f

---
+++ merengueproj/merengue/collab/migrations/0002_transmeta_fields.py
@@ -0,0 +1,164 @@
+
+from south.db import db
+from django.db import models
+from merengue.collab.models import *
+
+class Migration:
+    
+    def forwards(self, orm):
+        
+        # Deleting field 'CollabCommentRevisorStatusType.label'
+        db.delete_column('collab_collabcommentrevisorstatustype', 'label')
+        
+        # Deleting field 'CollabCommentUserType.labels'
+        db.delete_column('collab_collabcommentusertype', 'label')
+
+        # Deleting field 'CollabCommentRevisorStatusType.reason'
+        db.delete_column('collab_collabcommentrevisorstatustype', 'reason')
+        
+        # Adding field 'CollabCommentRevisorStatusType.label_fr'
+        db.add_column('collab_collabcommentrevisorstatustype', 'label_fr', orm['collab.CollabCommentRevisorStatusType:label_fr'])
+        
+        # Adding field 'CollabCommentRevisorStatusType.label_en'
+        db.add_column('collab_collabcommentrevisorstatustype', 'label_en', orm['collab.CollabCommentRevisorStatusType:label_en'])
+        
+        # Adding field 'CollabCommentUserType.label_es'
+        db.add_column('collab_collabcommentusertype', 'label_es', orm['collab.CollabCommentUserType:label_es'])
+        
+        # Adding field 'CollabCommentUserType.label_fr'
+        db.add_column('collab_collabcommentusertype', 'label_fr', orm['collab.CollabCommentUserType:label_fr'])
+        
+        # Adding field 'CollabCommentRevisorStatusType.reason_fr'
+        db.add_column('collab_collabcommentrevisorstatustype', 'reason_fr', orm['collab.CollabCommentRevisorStatusType:reason_fr'])
+        
+        # Adding field 'CollabCommentRevisorStatusType.reason_en'
+        db.add_column('collab_collabcommentrevisorstatustype', 'reason_en', orm['collab.CollabCommentRevisorStatusType:reason_en'])
+        
+        # Adding field 'CollabCommentUserType.label_en'
+        db.add_column('collab_collabcommentusertype', 'label_en', orm['collab.CollabCommentUserType:label_en'])
+        
+        # Adding field 'CollabCommentRevisorStatusType.label_es'
+        db.add_column('collab_collabcommentrevisorstatustype', 'label_es', orm['collab.CollabCommentRevisorStatusType:label_es'])
+        
+        # Adding field 'CollabCommentRevisorStatusType.reason_es'
+        db.add_column('collab_collabcommentrevisorstatustype', 'reason_es', orm['collab.CollabCommentRevisorStatusType:reason_es'])
+        
+    
+    
+    def backwards(self, orm):
+        
+        # Deleting field 'CollabCommentRevisorStatusType.label_fr'
+        db.delete_column('collab_collabcommentrevisorstatustype', 'label_fr')
+        
+        # Deleting field 'CollabCommentRevisorStatusType.label_en'
+        db.delete_column('collab_collabcommentrevisorstatustype', 'label_en')
+        
+        # Deleting field 'CollabCommentUserType.label_es'
+        db.delete_column('collab_collabcommentusertype', 'label_es')
+        
+        # Deleting field 'CollabCommentUserType.label_fr'
+        db.delete_column('collab_collabcommentusertype', 'label_fr')
+        
+        # Deleting field 'CollabCommentRevisorStatusType.reason_fr'
+        db.delete_column('collab_collabcommentrevisorstatustype', 'reason_fr')
+        
+        # Deleting field 'CollabCommentRevisorStatusType.reason_en'
+        db.delete_column('collab_collabcommentrevisorstatustype', 'reason_en')
+        
+        # Deleting field 'CollabCommentUserType.label_en'
+        db.delete_column('collab_collabcommentusertype', 'label_en')
+        
+        # Deleting field 'CollabCommentRevisorStatusType.label_es'
+        db.delete_column('collab_collabcommentrevisorstatustype', 'label_es')
+        
+        # Deleting field 'CollabCommentRevisorStatusType.reason_es'
+        db.delete_column('collab_collabcommentrevisorstatustype', 'reason_es')
+        
+        # Adding field 'CollabCommentRevisorStatusType.label'
+        db.add_column('collab_collabcommentrevisorstatustype', 'label', orm['collab.CollabCommentRevisorStatusType:label'])
+    
+        # Adding field 'CollabCommentUserType.label'
+        db.add_column('collab_collabcommentusertype', 'label', orm['collab.CollabCommentUserType:label'])
+        
+        # Adding field 'CollabCommentRevisorStatusType.reason'
+        db.add_column('collab_collabcommentrevisorstatustype', 'reason', orm['collab.CollabCommentRevisorStatusType:reason'])
+        
+    
+    models = {
+        'auth.group': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'unique_together': "(('content_type', 'codename'),)"},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'collab.collabcomment': {
+            'comment': ('django.db.models.fields.TextField', [], {}),
+            'comment_user_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['collab.CollabCommentUserType']"}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'content_type_set_for_collabcomment'", 'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ip_address': ('django.db.models.fields.IPAddressField', [], {'max_length': '15', 'null': 'True', 'blank': 'True'}),
+            'object_pk': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'submit_date': ('django.db.models.fields.DateTimeField', [], {'default': 'None', 'auto_now_add': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'collabcomment_comments'", 'null': 'True', 'to': "orm['auth.User']"}),
+            'user_email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'user_name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+            'user_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'})
+        },
+        'collab.collabcommentrevisorstatus': {
+            'comment': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['collab.CollabComment']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'revision_date': ('django.db.models.fields.DateTimeField', [], {'default': 'None', 'auto_now_add': 'True', 'blank': 'True'}),
+            'revisor': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'revised_collabcommentrevisorstatus_comments'", 'null': 'True', 'to': "orm['auth.User']"}),
+            'short_comment': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['collab.CollabCommentRevisorStatusType']"})
+        },
+        'collab.collabcommentrevisorstatustype': {
+            'decorator': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'label_en': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'label_es': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'label_fr': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'reason_en': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'reason_es': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'reason_fr': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'result': ('django.db.models.fields.CharField', [], {'default': "'just'", 'max_length': '30'})
+        },
+        'collab.collabcommentusertype': {
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'label_en': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'label_es': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'label_fr': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'unique_together': "(('app_label', 'model'),)", 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+    
+    complete_apps = ['collab']
