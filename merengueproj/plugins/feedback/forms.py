e215d62d14ec32f398d06b25eaeac71e8a89b29e
ant30 <ant30tx@gmail.com>
2009-09-22 11:35:26 +0000

    See #65 Fixing some styles to use with yaco theme
    git-svn-id: https://svn.merengueproject.org/trunk@351 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/feedback/forms.py
+++ merengueproj/plugins/feedback/forms.py
@@ -2,7 +2,7 @@ from merengue.base.forms import BaseModelForm
 
 from threadedcomments.models import FreeThreadedComment
 from threadedcomments.forms import FreeThreadedCommentForm
-from captcha.fields import CaptchaField
+#from captcha.fields import CaptchaField
 
 
 class CaptchaFreeThreadedCommentForm(BaseModelForm, FreeThreadedCommentForm):
@@ -13,7 +13,8 @@ class CaptchaFreeThreadedCommentForm(BaseModelForm, FreeThreadedCommentForm):
 
     def __init__(self, user, *args, **kwargs):
         super(CaptchaFreeThreadedCommentForm, self).__init__(*args, **kwargs)
-        if user.is_anonymous():
-            captcha_field = CaptchaField()
-            self.fields['captcha'] = captcha_field
-            self.declared_fields['captcha'] = captcha_field
+#        if user.is_anonymous():
+#            captcha_field = CaptchaField()
+#            self.fields['captcha'] = captcha_field
+#            self.declared_fields['captcha'] = captcha_field
+        pass
