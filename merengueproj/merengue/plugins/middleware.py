d233e762efc700751cf8cf7ea79448ef84fb1497
msaelices <msaelices@yaco.es>
2009-09-28 21:21:44 +0000

    Reset all catalogs when loading plugins. Fixes #139.
    git-svn-id: https://svn.merengueproject.org/trunk@398 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/plugins/middleware.py
+++ merengueproj/merengue/plugins/middleware.py
@@ -1,4 +1,7 @@
+from django.conf import settings
 from django.core.cache import cache
+
+from django.utils import translation
 
 
 class ActivePluginsMiddleware(object):
@@ -10,11 +13,19 @@ class ActivePluginsMiddleware(object):
             from merengue.plugins.utils import (enable_plugin, disable_plugin,
                                              get_plugin_module_name)
             from merengue.plugins.models import RegisteredPlugin
+            # enable active plugins
             active_plugins = RegisteredPlugin.objects.actives()
             plugin_names = [get_plugin_module_name(p.directory_name)
                             for p in active_plugins]
             for plugin_name in plugin_names:
                 enable_plugin(plugin_name, register=True)
+            # reset all i18n catalogs to load plugin ones
+            if settings.USE_I18N:
+                lang = translation.get_language()
+                translation.trans_real._translations = {}
+                translation.deactivate()
+                translation.activate(lang)
+            # deactivate inactive plugins
             inactive_plugins = RegisteredPlugin.objects.inactives()
             for plugin in inactive_plugins:
                 plugin_name = get_plugin_module_name(plugin.directory_name)
