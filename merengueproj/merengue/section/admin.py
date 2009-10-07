301c552803aafd63c75b81dec4d3d6e2ad7e5349
Emilio A. Sanchez Lopez <esanchez@cymoril>
2009-10-07 09:10:34 +0000

    Uncomment commented model admins. Fixes #157
    git-svn-id: https://svn.merengueproject.org/trunk@513 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/section/admin.py
+++ merengueproj/merengue/section/admin.py
@@ -437,6 +437,5 @@ def register(site):
     site.register_related(Document, DocumentRelatedModelAdmin, related_to=Section)
     site.register_related(CustomStyle, CustomStyleRelatedModelAdmin, related_to=Section)
     site.register_related(Menu, MainMenuRelatedAdmin, related_to=Section)
-    # commented for problems with previous register_related call (see ticket #157)
-    #site.register_related(Menu, SecondaryMenuRelatedAdmin, related_to=Section)
+    site.register_related(Menu, SecondaryMenuRelatedAdmin, related_to=Section)
     site.register_related(DocumentSection, DocumentSectionRelatedModelAdmin, related_to=Document)
