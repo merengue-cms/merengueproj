07efb20eca2083665e89c895b58a1f42ad4cd5dc
msaelices <msaelices@yaco.es>
2009-10-15 16:29:55 +0000

    Deleted not used urls. See #1.
    git-svn-id: https://svn.merengueproject.org/trunk@533 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/base/urls.py
+++ merengueproj/merengue/base/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls.defaults import patterns
 from django.contrib import admin
 
 admin.autodiscover()
@@ -6,9 +6,4 @@ admin.autodiscover()
 urlpatterns = patterns('merengue.base.views',
     (r'^admin_redirect/(?P<content_type>\d+)/(?P<content_id>\d+)/(?P<url>.*)$', 'admin_link'),
     (r'^public_redirect/(?P<app_label>\w+)/(?P<model_name>\w+)/(?P<content_id>\d+)/$', 'public_link'),
-    url(r'^enviar_comentario/(?P<content_type>\d+)/(?P<content_id>\d+)/(?P<parent_id>\d+)/$',
-        'content_comment_add', name='content_comment_add_parent'),
-    url(r'^enviar_comentario/(?P<content_type>\d+)/(?P<content_id>\d+)/$', 'content_comment_add', name='content_comment_add'),
-    url(r'^change_comment_visiblity/(?P<comment_id>\d+)/$', 'content_comment_change_visibity', name='content_comment_change_visibity'),
-    url(r'^content_comment_delete/(?P<comment_id>\d+)/$', 'content_comment_delete', name='content_comment_delete'),
 )
