01bb802fd01d8d47bfdf90188a55677f6e61a474
msaelices <msaelices@yaco.es>
2009-10-30 08:22:02 +0000

    Priscilla application created by startapp project. See #183.
    git-svn-id: https://svn.merengueproject.org/trunk@580 5c12d508-d76f-0410-91c7-e3e856e0eb0f

---
+++ merengueproj/plugins/ezdashboard/config.py
@@ -0,0 +1,35 @@
+from merengue.plugins import Plugin
+
+from plugins.news.actions import PDFExport, NewsIndex, NewsRSS
+from plugins.news.blocks import LatestNewsBlock, NewsCommentsBlock
+
+from plugins.news.models import NewsItem
+from plugins.news.admin import NewsItemSectionAdmin
+
+
+class PluginConfig(Plugin):
+    name = 'News'
+    description = 'News plugin'
+    version = '0.0.1a'
+#    required_apps = ('django.contrib.flatpages', )
+#    required_plugins = {
+#        'event': {
+#            'name': 'Events',
+#            'version': '0.0.1a',
+#        },
+#    }
+    url_prefixes = (
+        ('news', 'plugins.news.urls'),
+    )
+
+    @classmethod
+    def get_actions(cls):
+        return [PDFExport, NewsIndex, NewsRSS]
+
+    @classmethod
+    def get_blocks(cls):
+        return [LatestNewsBlock, NewsCommentsBlock]
+
+    @classmethod
+    def section_models(cls):
+        return [(NewsItem, NewsItemSectionAdmin)]
