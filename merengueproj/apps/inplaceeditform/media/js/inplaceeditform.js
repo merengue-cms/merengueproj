1b7f2bfd0e831012430525f67cb7ba21eaf37f03
Pablo Martín <goinnn@gmail.com>
2009-10-31 21:27:55 +0000

    Fixes #181 Refactor inplceeditform
    git-svn-id: https://svn.merengueproject.org/trunk@598 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/apps/inplaceeditform/media/js/inplaceeditform.js
+++ merengueproj/apps/inplaceeditform/media/js/inplaceeditform.js
@@ -9,12 +9,8 @@ function inplaceeditform_ready($){
         $(this).hide();
         var tools = $(this).next('.inplace-tools');
         tools.show();
-        var tools_error = tools.find('.inplace-tools-error')[0];
-        var child_nodes = tools_error.childNodes;
-        for (i=0; i<child_nodes.length; i++)
-        {
-            tools_error.removeChild(child_nodes[i]);
-        }
+        var tools_error = tools.find('.inplace-tools-error');
+        tools_error.children().remove();
     });
 
     $('.inplace-tools .cancel').click(function (){
@@ -24,78 +20,76 @@ function inplaceeditform_ready($){
     });
 
     $('.inplace-tools .apply').click(function (){
+        var _self = this;
         var auto_id = $(this).parent().find('.field_id').html();
         var field_name = $(this).parent().find('.field_name').html();
         var obj_id = $(this).parent().find('.obj_id').html();
         var content_type_id = $(this).parent().find('.content_type_id').html();
         var form = $(this).parent().find('.form').html();
         var filters = $.evalJSON($(this).parent().find('.filters').html());
-
         var value_input = $(this).parent().find('#' + auto_id);
+        
         var value;
-
         if (value_input.attr('multiple'))
         {
             var options_selected = value_input.find('option:selected');
             value = [];
-            for( i =0; i< options_selected.length; i++)
-            {
+            $.each(options_selected, function (i, val) {
                 value[i] = options_selected[i].value;
-            }
+            });
         }
         else{
             value = value_input[0].value;
         }
+
         var form_query = '';
         if (form)
         {
             form_query = '&form='+form;
         }
-        var _self = this; 
-        var data = 'id='+obj_id+'&field='+field_name+'&value='+encodeURIComponent($.toJSON(value))+'&content_type_id='+content_type_id+form_query+'&'+'filters='+$.toJSON(filters);
-        $.ajax({
-        data: data,
-        url: "/inplaceeditform/",
-        type: "POST",
-        async:true,
-        success: function(response){
-            response = eval("("+ response +")");
-            if (response.errors)
-            {   
-                var tools_error = $(_self).parent().find('.inplace-tools-error')[0];
-                var child_nodes = tools_error.childNodes;
-                for (i=0; i<child_nodes.length; i++)
-                {
-                    tools_error.removeChild(child_nodes[i]);
-                }
-                var ul = document.createElement('ul');
-                ul.className = "errors";
-                tools_error.appendChild(ul);
-                for (var error in response)
-                {
-                    if (error != 'errors')
-                    {
-                        var li = document.createElement('li');
-                        if ("'+field_name+'" == error)
-                            li.innerHTML = response[error];
-                        else
-                            li.innerHTML = error+ ": " +response[error];
-                        ul.appendChild(li);
-                    }
-                }
-            }
-            else
-            {
 
-                var inplace_tools = $(_self).parent();
-                inplace_tools.hide();
-                inplace_tools.prev('.inplace-view-editable-field').html(response.value)
-                inplace_tools.prev('.inplace-view-editable-field').show();
-                var inplace_messages = inplace_tools.prev().prev('.inplace-messages');    
-                inplace_messages.fadeIn();
-                window.setTimeout("jQuery('.inplace-messages').fadeOut()", 2000);
+        
+        var data = 'id=' + obj_id + '&field=' + field_name + '&value=' + encodeURIComponent($.toJSON(value)) +
+                   '&content_type_id=' + content_type_id+form_query + '&'+'filters=' + $.toJSON(filters);
+        $.ajax({
+            data: data,
+            url: "/inplaceeditform/",
+            type: "POST",
+            async:true,
+            success: function(response){
+                response = eval("("+ response +")");
+                if (response.errors)
+                {   
+                    var tools_error = $(_self).parent().find('.inplace-tools-error');
+                    tools_error.children().remove();
+                    var html = '<ul class="errors">';
+                    
+                    for (var error in response)
+                    {
+                        if (error != 'errors')
+                        {
+                            html += '<li>' ;
+                            if ("'+field_name+'" == error)
+                                html += response[error];
+                            else
+                                html += error+ ": " +response[error];
+                            html += '</li>'
+                        }
+                    }
+                    html += "</ul>"
+                    tools_error.html(html);
+                }
+                else
+                {
+                    var inplace_tools = $(_self).parent();
+                    inplace_tools.hide();
+                    inplace_tools.prev('.inplace-view-editable-field').html(response.value)
+                    inplace_tools.prev('.inplace-view-editable-field').show();
+                    var inplace_messages = inplace_tools.prev().prev('.inplace-messages');    
+                    inplace_messages.fadeIn();
+                    window.setTimeout("jQuery('.inplace-messages').fadeOut()", 2000);
+                }
             }
-        }
-    });
+        });
     });
 }
