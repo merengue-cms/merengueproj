f0df273748e292add9b50bec2d2cb9a6477beace
Fidel Ramos <fidelramos@gmail.com>
2009-11-09 10:13:00 +0000

    Ver #192. Template que faltaba para la templatetag videoplayer.py.
    git-svn-id: https://svn.merengueproject.org/trunk@620 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/tv/templatetags/videoplayer.py
+++ merengueproj/plugins/tv/templatetags/videoplayer.py
@@ -4,7 +4,7 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag('tv/inc.video_player_js.html', takes_context=True)
+@register.inclusion_tag('tv/inc.video_player_js.html')
 def videoplayerjs(video):
     """Loads the required media an libraries for the video player. This
     should be used in the template <head>."""
@@ -12,7 +12,7 @@ def videoplayerjs(video):
             'clipid': video.clipid}
 
 
-@register.inclusion_tag('tv/inc.video_player.html', takes_context=True)
+@register.inclusion_tag('tv/inc.video_player.html')
 def videoplayer(video):
     """Embeds a video player. This should be used in the template <body>."""
     return {'channel': video.channel,
