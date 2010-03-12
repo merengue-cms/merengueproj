e36e4ae0f1bad5b521a1c189ec057b8aeb168e91
Javier de la Rosa <versae@gmail.com>
2010-03-12 11:21:41 +0000

    Ver #183. Added new settings and cleaned HTML code.
    git-svn-id: https://svn.merengueproject.org/trunk@792 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/ezdashboard/config.py
+++ merengueproj/plugins/ezdashboard/config.py
@@ -6,14 +6,26 @@ from merengue.registry import params
 
 class PluginConfig(Plugin):
     name = 'EzDashboard'
-    description = 'Dashboard for users plugins like iGoogle, based on EzWeb platform'
-    version = '0.0.1a'
+    description = 'Dashboard for users plugins like iGoogle, based on ' \
+                  'EzWeb platform'
+    version = '0.0.2a'
     url_prefixes = (
         ('dashboard', 'plugins.ezdashboard.urls'),
     )
     config_params = [
         params.Single(name='theme', label=_('EzWeb theme'), default='default'),
-        params.Single(name='ezweburl', label=_('EzWeb base URL'), default='http://ezweb.yaco.es/'),
+        params.Single(name='url', label=_('EzWeb base URL'),
+                      default='http://ezweb.yaco.es/'),
+        params.Single(name='style', label=_('EzWeb iframe CSS style'),
+                      default='border: none;'),
+        params.Single(name='class', label=_('EzWeb iframe CSS class'),
+                      default=''),
+        params.Single(name='width', label=_('EzWeb iframe width'),
+                      default='100%'),
+        params.Single(name='height', label=_('EzWeb iframe height'),
+                      default='500px'),
+        params.Single(name='lite', label=_('EzWeb lite'),
+                      default=True),
     ]
 
     #@classmethod
