4edecd1708c35b054de3b16ce9ab5c0d9f74f363
msaelices <msaelices@yaco.es>
2010-04-14 09:54:09 +0000

    Upgraded south to 0.7 version. See #337. no-pyflakes. no-pep8.
    git-svn-id: https://svn.merengueproject.org/trunk@848 5c12d508-d76f-0410-91c7-e3e856e0eb0f

---
+++ merengueproj/apps/south/introspection_plugins/__init__.py
@@ -0,0 +1,9 @@
+# This module contains built-in introspector plugins for various common
+# Django apps.
+
+# These imports trigger the lower-down files
+import south.introspection_plugins.geodjango
+import south.introspection_plugins.django_tagging
+import south.introspection_plugins.django_taggit
+import south.introspection_plugins.django_objectpermissions
+
