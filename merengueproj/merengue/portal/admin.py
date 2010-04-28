5eb491c6ea137d878b81237c669cc6bccceb6bf1
danigm <danigm@wadobo.com>
2010-04-28 15:52:02 +0000

    Added autofill for link slug. See #397
    git-svn-id: https://svn.merengueproject.org/trunk@970 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/portal/admin.py
+++ merengueproj/merengue/portal/admin.py
@@ -4,6 +4,7 @@ from merengue.portal.models import PortalLink
 
 class PortalLinkAdmin(BaseAdmin):
     list_display = BaseAdmin.list_display + ('category', )
+    prepopulated_fields = {'slug': ('name_es', )}
 
 
 def register(site):
