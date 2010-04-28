eaf1ed8f4a221b1675a0fa5241dddeaeb8f64bb0
Pablo Martín <goinnn@gmail.com>
2010-04-28 16:19:59 +0000

    See #393 More utilities of news. A ajax view
    git-svn-id: https://svn.merengueproject.org/trunk@976 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/news/urls.py
+++ merengueproj/plugins/news/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('plugins.news.views',
     url(r'^$', 'news_index', name='news_index'),
     url(r'^(?P<newsitem_slug>[\w-]+)/$', 'newsitem_view', name='newsitem_view'),
+    url(r'^ajax/(?P<newscategory_slug>[\w-]+)/$', 'newsitem_by_category_view', name='newsitem_by_category_view'),
 )
--- merengueproj/plugins/news/views.py
+++ merengueproj/plugins/news/views.py
@@ -1,7 +1,10 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
+from django.template import RequestContext
+from django.template.loader import render_to_string
 
 from merengue.base.views import content_view, content_list
-from plugins.news.models import NewsItem
+from plugins.news.models import NewsItem, NewsCategory
 
 
 def news_index(request):
@@ -12,3 +15,14 @@ def news_index(request):
 def newsitem_view(request, newsitem_slug):
     newsitem = get_object_or_404(NewsItem, slug=newsitem_slug)
     return content_view(request, newsitem, 'news/newsitem_view.html')
+
+
+def newsitem_by_category_view(request, newscategory_slug):
+    if request.is_ajax():
+        newscategory = NewsCategory.objects.get(slug=newscategory_slug)
+        news_string = render_to_string('news/newsitem_by_category.html',
+                                       {'news_category_active': newscategory,
+                                       },
+                                       context_instance=RequestContext(request))
+        return HttpResponse(news_string, mimetype='txt/html')
+    return HttpResponseRedirect('/')
