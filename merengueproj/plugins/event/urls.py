bfc7214f7a3e2555d3d76e7a2dec85872e956ffa
msaelices <msaelices@yaco.es>
2010-04-29 19:21:13 +0000

    Follow the cleaning. See #411.
    git-svn-id: https://svn.merengueproject.org/trunk@1023 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/event/urls.py
+++ merengueproj/plugins/event/urls.py
@@ -2,10 +2,5 @@ from django.conf.urls.defaults import patterns, url
 
 
 urlpatterns = patterns('plugins.event.views',
-    url(r'^$', 'event_index', name='event_index'),
-    url(r'^top-rated/$', 'top_events_rated', name='event_top_rated'),
-    url(r'^search/$', 'event_search', name='event_search'),
-    url(r'^search/quick/$', 'event_quick_search', name='event_quick_search'),
-    url(r'^search/advanced/$', 'event_advanced_search', name='event_advanced_search'),
     url(r'^(?P<event_slug>[\w-]+)/$', 'event_view', name='event_view'),
 )
