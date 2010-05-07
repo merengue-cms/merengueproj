8a10d6669f3102a27bfe58d10613fa1db6fc5792
danigm <danigm@wadobo.com>
2010-05-07 12:25:11 +0000

    Added a plugin config to event for change the tooltip border color. Close #424
    git-svn-id: https://svn.merengueproject.org/trunk@1115 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/event/blocks.py
+++ merengueproj/plugins/event/blocks.py
@@ -2,6 +2,7 @@ from datetime import date
 
 from django.utils.translation import ugettext as _
 
+from merengue.registry import params
 from merengue.block.blocks import Block
 
 from plugins.event.utils import getEventsMonthYear
@@ -11,12 +12,18 @@ class EventsCalendarBlock(Block):
     name = 'eventscalendar'
     default_place = 'rightsidebar'
 
+
+    config_params = [params.Single(name='calendar_tooltip_color', label=_('calendar tooltip color'), default='black')]
+
     @classmethod
     def render(cls, request, place):
+        calendar_tooltip_color = cls.get_config()['calendar_tooltip_color'].get_value()
+
         current_month = date.today().month
         current_year = date.today().year
         events_dic = getEventsMonthYear(current_month, current_year)
         return cls.render_block(request,
                                 template_name='event/block_calendar.html',
                                 block_title=_('Events calendar'),
-                                context={'events_dic': events_dic})
+                                context={'events_dic': events_dic,
+                                         'calendar_tooltip_color': calendar_tooltip_color})
