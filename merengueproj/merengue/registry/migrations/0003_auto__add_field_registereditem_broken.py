6fb831c346f44ac1fbe2955eb4d2c9299ea42bba
msaelices <msaelices@yaco.es>
2010-05-10 18:12:09 +0000

    Migration for broken registered items checking. Fixes #431. no-pyflakes. no-pep8.
    git-svn-id: https://svn.merengueproject.org/trunk@1147 5c12d508-d76f-0410-91c7-e3e856e0eb0f

---
+++ merengueproj/merengue/registry/migrations/0003_auto__add_field_registereditem_broken.py
@@ -0,0 +1,30 @@
+# encoding: utf-8
+from south.db import db
+from south.v2 import SchemaMigration
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'RegisteredItem.broken'
+        db.add_column('registry_registereditem', 'broken', self.gf('django.db.models.fields.BooleanField')(default=False, blank=True), keep_default=False)
+
+    def backwards(self, orm):
+        # Deleting field 'RegisteredItem.broken'
+        db.delete_column('registry_registereditem', 'broken')
+
+    models = {
+        'registry.registereditem': {
+            'Meta': {'object_name': 'RegisteredItem'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'broken': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'category': ('django.db.models.fields.CharField', [], {'max_length': '100', 'db_index': 'True'}),
+            'class_name': ('django.db.models.fields.CharField', [], {'max_length': '100', 'db_index': 'True'}),
+            'config': ('merengue.registry.dbfields.ConfigField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'module': ('django.db.models.fields.CharField', [], {'max_length': '200', 'db_index': 'True'}),
+            'order': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['registry']
