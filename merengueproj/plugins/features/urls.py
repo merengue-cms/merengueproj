596dd5816efdefb98b5102fb198d715d3554030a
msaelices <msaelices@yaco.es>
2010-05-10 10:26:41 +0000

    Moved "features" plugin to right location. "features" plugin is a merengueproject.org plugin, not a generic one. See #403. no-pyflakes.
    git-svn-id: https://svn.merengueproject.org/trunk@1129 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/features/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-
-urlpatterns = patterns('plugins.features.views',
-    url(r'^$', 'features_index', name='features_index'),
-    url(r'^(?P<features_slug>[\w-]+)/$', 'features_view',
-        name='features_view'),
-)
