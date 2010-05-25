1266b6af584d7e0f023edf389cbe67b975235eee
Emilio A. Sanchez Lopez <esanchez@cymoril>
2010-05-25 07:49:33 +0000

    Change object tools related admin title and behaviour when clicking it. Fixes #353
    git-svn-id: https://svn.merengueproject.org/trunk@1284 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/media/js/jquery.smart_relations_object_tool.js
+++ merengueproj/merengue/media/js/jquery.smart_relations_object_tool.js
@@ -4,6 +4,16 @@
             var container = $(this);
             var size = container.find('li').length
             var active = $(this).find('li.parent');
+
+            var toggleMenu = function() {
+                container.toggle();
+                $('.din_smart_relations_object_tool').not(container).hide();
+                var trigger = $(this).parent('li').find('.din_smart_trigger');
+                $('.din_smart_trigger').not(trigger).removeClass('din_smart_trigger_open');
+                trigger.toggleClass('din_smart_trigger_open');
+                return false;
+            };
+
             container.addClass('din_smart_relations_object_tool'); 
             if (active.length) {
                 container.after(active);
@@ -15,14 +25,7 @@
             if (size>1) {
                 active.addClass('din_selected');
                 active.append('<a href="" class="din_smart_trigger launch_smart_trigger"></a>');
-                active.find('a.launch_smart_trigger').click(function() {
-                    container.toggle();
-                    $('.din_smart_relations_object_tool').not(container).hide();
-                    var trigger = $(this).parent('li').find('.din_smart_trigger');
-                    $('.din_smart_trigger').not(trigger).removeClass('din_smart_trigger_open');
-                    trigger.toggleClass('din_smart_trigger_open');
-                    return false;
-                });
+                active.find('a').click(toggleMenu);
             }
         });
     };
