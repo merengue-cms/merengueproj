424a167ed49b9dbd98c4b2ec118a2018cadd50bb
Emilio Jiménez <jimenezmoral@gmail.com>
2010-06-11 12:16:10 +0000

    Add roles permissions admin view. See #383 no-pyflakes
    git-svn-id: https://svn.merengueproject.org/trunk@1494 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/perms/admin.py
+++ merengueproj/merengue/perms/admin.py
@@ -1,9 +1,96 @@
+from django.conf.urls.defaults import *
+from django.contrib import admin
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.utils.translation import ugettext as _
+
 from merengue.base.admin import BaseAdmin
 
 from merengue.perms.models import ObjectPermission
 from merengue.perms.models import Permission
 from merengue.perms.models import PrincipalRoleRelation
 from merengue.perms.models import Role
+
+
+class ObjectPermissionAdmin(admin.ModelAdmin):
+
+    def has_add_permission(self, request):
+        return False
+
+    def get_urls(self):
+        urls = super(ObjectPermissionAdmin, self).get_urls()
+        # override objectpermissions root path
+        my_urls = patterns('',
+            (r'^$', self.admin_site.admin_view(self.change_roles_permissions)))
+
+        return my_urls + urls
+
+    def change_roles_permissions(self, request):
+
+        opts = self.model._meta
+        admin_site = self.admin_site
+        has_perm = request.user.has_perm(opts.app_label + '.' + opts.get_change_permission())
+
+        if request.method == 'POST':
+            selected = request.POST.getlist('selected_perm')
+            for obj_perm in ObjectPermission.objects.filter(content__isnull=True):
+                role_perm = "%s_%s" % (obj_perm.role.id, obj_perm.permission.id)
+                if role_perm not in selected:
+                    obj_perm.delete()
+
+            for role_perm in selected:
+                role_id, perm_id = role_perm.split('_')
+                role = Role.objects.get(id=role_id)
+                perm = Permission.objects.get(id=perm_id)
+                op, created = ObjectPermission.objects.get_or_create(role=role, permission=perm, content=None)
+
+            return self.response_change(request)
+
+
+        roles = Role.objects.all()
+        permissions = {}
+
+        for perm in Permission.objects.all():
+            permissions[perm] = []
+            for role in roles:
+                permissions[perm].append((role, perm.objectpermission_set.filter(role=role) and True or False))
+
+
+        context = {'admin_site': admin_site.name,
+                   'change': True,
+                   'is_popup': False,
+                   'save_as': False,
+                   'has_delete_permission': False,
+                   'has_add_permission': False,
+                   'add': False,
+                   'model_admin': self,
+                   'title': "Roles permissions",
+                   'opts': opts,
+                   'root_path': '/%s' % admin_site.root_path,
+                   'app_label': opts.app_label,
+                   'has_change_permission': has_perm,
+                   'role_permissions': permissions,
+                   'roles': roles}
+
+
+        template = 'admin/perms/objectpermission/role_permissions.html'
+        return render_to_response(template,
+                                  context,
+                                  context_instance=RequestContext(request))
+
+    def response_change(self, request):
+        """
+        Determines the HttpResponse for the change_view stage.
+        """
+
+        msg = _('The role permissions was changed successfully.')
+        if "_continue" in request.POST:
+            self.message_user(request, msg + ' ' + _("You may edit it again below."))
+            return HttpResponseRedirect(request.path)
+        else:
+            self.message_user(request, msg)
+            return HttpResponseRedirect("../")
 
 
 class RoleAdmin(BaseAdmin):
@@ -35,7 +122,7 @@ class RoleAdmin(BaseAdmin):
 
 
 def register(site):
-    site.register(ObjectPermission)
+    site.register(ObjectPermission, ObjectPermissionAdmin)
     site.register(Permission)
     site.register(Role, RoleAdmin)
     site.register(PrincipalRoleRelation)
