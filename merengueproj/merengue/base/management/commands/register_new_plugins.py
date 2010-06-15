8e29f0b71cefc19a08a5178af0ed49093398ba0e
msaelices <msaelices@yaco.es>
2010-06-15 17:45:00 +0000

    register_new_plugins command invalidates any possible cache with johnny cache. Fixes #673.
    git-svn-id: https://svn.merengueproject.org/trunk@1602 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/base/management/commands/register_new_plugins.py
+++ merengueproj/merengue/base/management/commands/register_new_plugins.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 import os
 from django.conf import settings
+from django.core.cache import cache as django_cache
 
 from merengue.base.management.base import MerengueCommand
 from merengue.pluggable import register_plugin
 from merengue.pluggable.utils import get_plugins_dir
+from merengue.pluggable.models import RegisteredPlugin
+
+from johnny import cache
 
 
 class Command(MerengueCommand):
@@ -14,3 +18,7 @@ class Command(MerengueCommand):
     def handle(self, **options):
         for plugin_dir in os.listdir(os.path.join(settings.BASEDIR, get_plugins_dir())):
             register_plugin(plugin_dir)
+        # cache invalidation
+        query_cache_backend = cache.get_backend()(django_cache)
+        query_cache_backend.patch()
+        cache.invalidate(RegisteredPlugin._meta.db_table)
