a561b7169a4691e17489821e1d82dafab57b90d5
Pablo Martín <goinnn@gmail.com>
2010-06-24 17:53:32 +0000

    Fixes #690 No error now in chrome. It doesn't work in IExplorer.
    '''Quality''': Check in "every" browser
    
    Quality ;-)
    
    
    
    git-svn-id: https://svn.merengueproject.org/trunk@1762 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/voting/media/js/jquery.rating.js
+++ merengueproj/plugins/voting/media/js/jquery.rating.js
@@ -6,27 +6,27 @@ $.fn.rating = function (feedback_msg) {
             var form = this;
 
             function ajax_submit() {
-                var button = $(this);
-                var vote = button.val();
-                var url = $(form).attr("action");
-                var csrf = $("input[name=csrfmiddlewaretoken]", form).val();
-                var data = {vote: vote,
-                            is_ajax: true,
-                            csrfmiddlewaretoken: csrf};
-                $(form).load(url, data, function() {
-                        $(".star-rating", form).children().appendTo(form)
-                                               .end().remove();
-                        if (feedback_msg) {
-                            $("ol", form).append('<li class="rating-feedback">' + feedback_msg + '</li>');
-                            var feedback = $(".rating-feedback", form);
-                            feedback.fadeOut(5000);
-                        }
-                        $("input[type=submit]", form).click(ajax_submit); 
-                    });
+                if($(this).hasClass("can_vote")){
+                    var button = $(this).find("input[type=submit]");
+                    var vote = button.val();
+                    var url = $(form).attr("action");
+                    var csrf = $("input[name=csrfmiddlewaretoken]", form).val();
+                    var data = {vote: vote,
+                                is_ajax: true,
+                                csrfmiddlewaretoken: csrf};
+                    $(form).load(url, data, function() {
+                            if (feedback_msg) {
+                                $("ol", form).append('<li class="rating-feedback">' + feedback_msg + '</li>');
+                                var feedback = $(".rating-feedback", form);
+                                feedback.fadeOut(5000);
+                            }
+                            $("ol li a", form).click(ajax_submit); 
+                        });
+                }
                 return false;
             };
 
-            $("input[type=submit]", form).click(ajax_submit); 
+            $("ol li a", form).click(ajax_submit); 
         });
 
 };
