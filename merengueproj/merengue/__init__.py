0c203de7cbc9d6bf94bbc9d9bcfc8031292f190c
msaelices <msaelices@yaco.es>
2010-07-21 16:20:40 +0000

    We create 0.5.1-beta-2 version. See #840.
    git-svn-id: https://svn.merengueproject.org/trunk@2116 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/__init__.py
+++ merengueproj/merengue/__init__.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Merengue.  If not, see <http://www.gnu.org/licenses/>.
 
-VERSION = (0, 5, 1, 'beta', 0)
+VERSION = (0, 5, 1, 'beta', 2)
 
 
 def get_version():
