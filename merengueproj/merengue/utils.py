65d571c5bac05a5d1e1a04b503cf72e34a0f7e7f
msaelices <msaelices@yaco.es>
2010-07-22 11:53:01 +0000

    Removed unused code that make fails Merengue when USE_GIS is True. Fixes #848.
    git-svn-id: https://svn.merengueproject.org/trunk@2136 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/utils.py
+++ merengueproj/merengue/utils.py
@@ -34,35 +34,6 @@ from django.db.models import get_models
 from django.db.models.loading import load_app
 
 
-if settings.USE_GIS:
-    from geopy import geocoders
-    from merengue.base.models import Location
-
-    def geolocate_object_base(object_base_content):
-        if not object_base_content.location:
-            object_base_content.location=Location.objects.create()
-        city = object_base_content.location.cities.all()
-        city = (city and city[0]) or ""
-        address = object_base_content.location.address or ""
-        number = object_base_content.location.number or ""
-        postal_code = object_base_content.location.postal_code or ""
-        address_type = ""
-        if object_base_content.location.address_type_id and object_base_content.location.address_type.name != 'Otros':
-            address_type = object_base_content.location.address_type.name or ""
-
-        query = u"%s %s %s %s %s, andalusia, spain" % (address_type, address, number, postal_code, city)
-        query = smart_str(query.strip())
-        g = geocoders.Google(settings.GOOGLE_MAPS_API_KEY, 'maps.google.es')
-        try:
-            place_google, (lat, lon) = g.geocode(query)
-            object_base_content.location.main_location = 'POINT(%f %f)' % (lon, lat)
-            object_base_content.location.save()
-            object_base_content.is_autolocated = True
-            object_base_content.save()
-        except ValueError, e:
-            pass
-
-
 def copy_request(request, delete_list, copy=None):
     from copy import deepcopy
     request_copy = copy and copy(request) or deepcopy(request)
