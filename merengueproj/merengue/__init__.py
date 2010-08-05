e53e2db4cd0e97bc275c9e343542427a6def8733
msaelices <msaelices@yaco.es>
2010-08-05 22:41:35 +0000

    Preparing Merengue 0.5.1-rc1 release. See #840. no-pep8
    git-svn-id: https://svn.merengueproject.org/trunk@2186 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/__init__.py
+++ merengueproj/merengue/__init__.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Merengue.  If not, see <http://www.gnu.org/licenses/>.
 
-VERSION = (0, 5, 1, 'beta', 3)
+VERSION = (0, 5, 1, 'rc', 1)
 
 
 def get_version():
