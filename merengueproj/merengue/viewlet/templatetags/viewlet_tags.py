6bfb4a6bb71be82b91cf869fd9dab03f57fd6ee8
Pablo Martín <goinnn@gmail.com>
2010-09-15 09:21:42 +0000

    Fixes #960 Catch PermissionDenied and Http404 in viewlets (403 and 404)
    git-svn-id: https://svn.merengueproject.org/trunk@2350 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/viewlet/templatetags/viewlet_tags.py
+++ merengueproj/merengue/viewlet/templatetags/viewlet_tags.py
@@ -16,7 +16,11 @@
 # along with Merengue.  If not, see <http://www.gnu.org/licenses/>.
 
 from django import template
-
+from django.conf import settings
+from django.template import RequestContext
+from django.template.loader import render_to_string
+from django.http import Http404
+from django.core.exceptions import PermissionDenied
 
 register = template.Library()
 
@@ -29,13 +33,17 @@ class RenderViewletNode(template.Node):
     def render(self, context):
         request = context.get('request', None)
         try:
-            if self.registered_viewlet is not None:
+            if self.registered_vielet is not None:
                 viewlet = self.registered_viewlet.resolve(context).get_registry_item_class()
             else:
                 viewlet = None
             return viewlet.render(request)
         except template.VariableDoesNotExist:
             return ''
+        except PermissionDenied:
+            return get_render_http_error(request, 403)
+        except Http404:
+            return get_render_http_error(request, 404)
 
 
 @register.tag(name='render_viewlet')
@@ -50,3 +58,13 @@ def do_render_viewlet(parser, token):
         raise template.TemplateSyntaxError('"%r" tag requires at one argument' % tag_name)
     registered_viewlet = parser.compile_filter(bits[1])
     return RenderViewletNode(registered_viewlet)
+
+
+# ----- auxiliar functions ----- #
+
+
+def get_render_http_error(request, http_error):
+    template = settings.HTTP_STATUS_CODE_TEMPLATES.get(http_error, None)
+    context = {'template_base': 'viewlet_error.html', 'is_admin': False}
+    return render_to_string(template, context,
+                                      context_instance=RequestContext(request))
