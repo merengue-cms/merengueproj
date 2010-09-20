741e711cb003e14e8ad92b08b05eed9761d1116c
Pablo Martín <goinnn@gmail.com>
2010-09-20 09:32:36 +0000

    Fixes #970 The url now are in english
    Fixes #971 The rename view response 200 Ok 
    
    
    git-svn-id: https://svn.merengueproject.org/trunk@2376 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/filebrowser/urls.py
+++ merengueproj/plugins/filebrowser/urls.py
@@ -3,16 +3,16 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('plugins.filebrowser.views',
     url(r'^(?P<repository_name>[\w-]+)/$', 'root', name='filebrowser_root'),
-    url(r'^(?P<repository_name>[\w-]+)/listado/$', 'listing', name='filebrowser_root_listing'),
-    url(r'^(?P<repository_name>[\w-]+)/listado/(?P<path>.*)$', 'listing', name="filebrowser_dir_listing"),
-    url(r'^(?P<repository_name>[\w-]+)/descarga/(?P<path>.*)$', 'download', name='filebrowser_download'),
-    url(r'^(?P<repository_name>[\w-]+)/subida/(?P<path>.*)$', 'upload', name='filebrowser_upload'),
-    url(r'^(?P<repository_name>[\w-]+)/crear/directorio/(?P<path>.*)$', 'createdir', name='filebrowser_createdir'),
-    url(r'^(?P<repository_name>[\w-]+)/accion/(?P<path>.*)$', 'action', name='filebrowser_action'),
+    url(r'^(?P<repository_name>[\w-]+)/listing/$', 'listing', name='filebrowser_root_listing'),
+    url(r'^(?P<repository_name>[\w-]+)/listing/(?P<path>.*)$', 'listing', name="filebrowser_dir_listing"),
+    url(r'^(?P<repository_name>[\w-]+)/download/(?P<path>.*)$', 'download', name='filebrowser_download'),
+    url(r'^(?P<repository_name>[\w-]+)/upload/(?P<path>.*)$', 'upload', name='filebrowser_upload'),
+    url(r'^(?P<repository_name>[\w-]+)/create/folder/(?P<path>.*)$', 'createdir', name='filebrowser_createdir'),
+    url(r'^(?P<repository_name>[\w-]+)/action/(?P<path>.*)$', 'action', name='filebrowser_action'),
 
     # documents
-    url(r'^crear/documento/(?P<path>.*)$', 'createdoc', name='filebrowser_createdoc'),
-    url(ur'^ver/documento/(?P<doc_slug>[-ÑñáéíóúÁÉÍÓÚ\w]+)/$', 'viewdoc', name='filebrowser_viewdoc'),
-    url(ur'^editar/documento/(?P<doc_slug>[-ÑñáéíóúÁÉÍÓÚ\w]+)/$', 'editdoc', name='filebrowser_editdoc'),
-    url(r'^borrar/(?P<type>[-\w]+)/adjunto_a/(?P<objId>[-\w]+)/$', 'remove_attachment', name='filebrowser_remove_attachment'),
+    url(r'^create/document/(?P<path>.*)$', 'createdoc', name='filebrowser_createdoc'),
+    url(ur'^view/document/(?P<doc_slug>[-ÑñáéíóúÁÉÍÓÚ\w]+)/$', 'viewdoc', name='filebrowser_viewdoc'),
+    url(ur'^edit/document/(?P<doc_slug>[-ÑñáéíóúÁÉÍÓÚ\w]+)/$', 'editdoc', name='filebrowser_editdoc'),
+    url(r'^remove/(?P<type>[-\w]+)/attachment/(?P<objId>[-\w]+)/$', 'remove_attachment', name='filebrowser_remove_attachment'),
 )
