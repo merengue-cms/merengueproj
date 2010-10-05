8dae4b39d6aadd65bff19a4ff758cc532f33cdf7
Pablo Martín <goinnn@gmail.com>
2010-10-05 14:26:45 +0000

    Cierra #995 Created a banner plugin
    git-svn-id: https://svn.merengueproject.org/trunk@2435 5c12d508-d76f-0410-91c7-e3e856e0eb0f

---
+++ merengueproj/plugins/banner/viewlets.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2010 by Yaco Sistemas <msaelices@yaco.es>
+#
+# This file is part of Merengue.
+#
+# Merengue is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Merengue is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Merengue.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.utils.translation import ugettext as _
+
+from merengue.viewlet.viewlets import Viewlet
+from plugins.banner.views import get_banners
+
+
+class AllBannerViewlet(Viewlet):
+    name = 'allbanner'
+    label = _('All Banners')
+
+    @classmethod
+    def render(cls, request):
+        banner_list = get_banners(request)
+        return cls.render_viewlet(request, template_name='banner/viewlet_latest.html',
+                                  context={'banner_list': banner_list,
+                                           'is_paginated': True,
+                                           'paginate_by': 10})
