ba55f63d1c2ba72fdea0387b17505bbfc7b4a065
Emilio A. Sanchez Lopez <esanchez@cymoril>
2010-10-14 17:59:45 +0000

    Do not allow debug_toolbar.urls.urlpatterns to grow wich each request. Fixes #286 no-pyflakes
    git-svn-id: https://svn.merengueproject.org/trunk@2473 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/apps/debug_toolbar/urls.py
+++ merengueproj/merengue/apps/debug_toolbar/urls.py
@@ -1,5 +1,5 @@
 """
-URLpatterns for the debug toolbar. 
+URLpatterns for the debug toolbar.
 
 These should not be loaded explicitly; the debug toolbar middleware will patch
 this into the urlconf for the request.
@@ -7,7 +7,9 @@ this into the urlconf for the request.
 from django.conf.urls.defaults import *
 from django.conf import settings
 
-urlpatterns = patterns('',
+base_urls = patterns('',
     url(r'^__debug__/m/(.*)$', 'debug_toolbar.views.debug_media'),
     url(r'^__debug__/sql_explain/$', 'debug_toolbar.views.sql_explain', name='sql_explain'),
 )
+
+urlpatterns = base_urls
