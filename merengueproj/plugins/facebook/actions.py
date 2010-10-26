b773657c24afca7beaad936c1d0b8e8cba0c1112
msaelices <msaelices@yaco.es>
2010-10-26 16:57:01 +0000

    Make facebook plugin works behind virtual hosts. Fixes #1013.
    git-svn-id: https://svn.merengueproject.org/trunk@2496 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/facebook/actions.py
+++ merengueproj/plugins/facebook/actions.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Merengue.  If not, see <http://www.gnu.org/licenses/>.
 
+from django.contrib.sites.models import Site
 from django.http import HttpResponseRedirect
 from django.utils.http import urlquote
 from django.utils.translation import ugettext_lazy as _
@@ -28,5 +29,5 @@ class FacebookLink(ContentAction):
 
     @classmethod
     def get_response(cls, request, content):
-        request_url = 'http://%s%s' % (request.get_host(), content.public_link())
+        request_url = 'http://%s%s' % (Site.objects.get_current().domain, content.public_link())
         return HttpResponseRedirect('http://www.facebook.com/share.php?u=%s' % urlquote(request_url))
