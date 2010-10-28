37e5caba8d0b7951a78f8d0deab1d0b480957faf
Pablo Recio <pablo@recio.me>
2010-10-28 14:47:47 +0000

    Added "odtexport" plugin.
    This plugin uses OOT application to render content base to an ODT file.
    
    Close #1017
    
    
    git-svn-id: https://svn.merengueproject.org/trunk@2499 5c12d508-d76f-0410-91c7-e3e856e0eb0f

---
+++ merengueproj/plugins/odtexport/config.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2010 by Yaco Sistemas <msaelices@yaco.es>
+#
+# This file is part of Merengue.
+#
+# Merengue is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Merengue is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Merengue.  If not, see <http://www.gnu.org/licenses/>.
+
+from merengue.pluggable import Plugin
+
+from plugins.odtexport.actions import ExportODT
+from plugins.odtexport.admin import OpenOfficeTemplateAdmin
+from plugins.odtexport.models import OpenOfficeTemplate
+
+
+class PluginConfig(Plugin):
+    name = 'OpenOfficeExport'
+    description = 'OpenOffice Exporter'
+    version = '0.0.2'
+
+    url_prefixes = (
+        ('odtexport', 'plugins.odtexport.urls'),
+    )
+
+    @classmethod
+    def get_actions(cls):
+        return [ExportODT]
+
+    @classmethod
+    def get_model_admins(cls):
+        return [(OpenOfficeTemplate, OpenOfficeTemplateAdmin)]
