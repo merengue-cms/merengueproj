e42443b682795fa9c3cf393fec838a23940251e1
Pablo Recio <pablo@recio.me>
2010-11-04 14:59:10 +0000

    Added static rendering of the location using google static map.
    See #1022
    
    
    git-svn-id: https://svn.merengueproject.org/trunk@2513 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/odtexport/templatetags/odtexport_templates.py
+++ merengueproj/plugins/odtexport/templatetags/odtexport_templates.py
@@ -6,3 +6,13 @@ register = Library()
 @register.inclusion_tag('odtexport/images_rendering.xml')
 def images_rendering(content):
     return {'content': content}
+
+
+@register.inclusion_tag('odtexport/render_gmap.xml')
+def render_gmap(content, height=500, weight=300, zoom=15):
+    base_url = 'http://maps.google.com/maps/api/staticmap'
+    coords = content.location.main_location.coords
+    args = 'center=%s,%s&markers=%s,%s' % ((coords[1], coords[0]) * 2)
+    args += '&zoom=%s&size=%sx%s&sensor=true' % (zoom, height, weight)
+
+    return {'gmap_url': '%s?%s' % (base_url, args)}
