9fe020b355719f9a10b4a286ebb2700cd2305b3b
Pablo Recio <pablo@recio.me>
2010-11-19 11:23:57 +0000

    Added templatetag to odtexport plugin to escape HTML codes.
    Close #1017
    
    
    git-svn-id: https://svn.merengueproject.org/trunk@2605 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/odtexport/templatetags/odtexport_templates.py
+++ merengueproj/plugins/odtexport/templatetags/odtexport_templates.py
@@ -1,4 +1,8 @@
+from htmlentitydefs import entitydefs
+
 from django.template import Library
+from django.template.defaultfilters import stringfilter
+
 
 register = Library()
 
@@ -16,3 +20,13 @@ def render_gmap(content, height=500, weight=300, zoom=15):
     args += '&zoom=%s&size=%sx%s&sensor=true' % (zoom, height, weight)
 
     return {'gmap_url': '%s?%s' % (base_url, args)}
+
+
+@stringfilter
+def escaping_all_text(content):
+    for html_code, utf8_value in entitydefs.iteritems():
+        content = content.replace("&%s;" % html_code,
+                                  utf8_value.decode('latin-1'))
+    return content
+
+register.filter('escaping_all_text', escaping_all_text)
