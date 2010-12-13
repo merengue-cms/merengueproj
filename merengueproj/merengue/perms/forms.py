06b9b9d6ac98daee269ea809295eb29d530f28c5
Pablo Recio <pablo@recio.me>
2010-12-13 10:19:25 +0000

    'Owner' role does not show as elegible role on User form.
    Close #1127
    
    
    git-svn-id: https://svn.merengueproject.org/trunk@2734 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/perms/forms.py
+++ merengueproj/merengue/perms/forms.py
@@ -36,7 +36,7 @@ class UserChangeForm(DjangoUserChangeForm):
         if user:
             self.fields['roles'].initial = [role.id for role in get_global_roles(user)]
 
-    roles = forms.ModelMultipleChoiceField(queryset=Role.objects.all(),
+    roles = forms.ModelMultipleChoiceField(queryset=Role.objects.exclude(name=u'Owner'),
                                            widget=FilteredSelectMultiple(_('Roles'), False),
                                            required=False)
 
