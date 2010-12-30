6f55e067da962bc32daca74d669bc51361e7939a
msaelices <msaelices@yaco.es>
2010-12-30 15:22:04 +0000

    Improved stdimage validation to check if image was deleted but field is required. Fixes #1193.
    git-svn-id: https://svn.merengueproject.org/trunk@2870 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/apps/stdimage/forms.py
+++ merengueproj/merengue/apps/stdimage/forms.py
@@ -1,3 +1,4 @@
+from django.forms import ValidationError
 from django.forms.fields import ImageField
 
 from stdimage.globals import DELETED
@@ -8,5 +9,7 @@ class StdImageFormField(ImageField):
     def clean(self, data, initial=None):
         if data != DELETED:
             return super(StdImageFormField, self).clean(data, initial)
+        elif self.required and data == DELETED:
+            raise ValidationError(self.error_messages['required'])
         else:
             return data
