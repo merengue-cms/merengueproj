37577f2b99a2bc784abc63f51d203d59f59cd967
Pablo Recio <pablo@recio.me>
2011-01-13 11:21:21 +0000

    Added first version of the ContetMenu plugin.
    This version simply add an admin page to create a {{ContentGroup}} model. This
    model is related with {{BaseContent}} in order to group those contents.
    
    See #1260
    
    
    git-svn-id: https://svn.merengueproject.org/trunk@2941 5c12d508-d76f-0410-91c7-e3e856e0eb0f

---
+++ merengueproj/plugins/contentmenu/config.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2010 by Yaco Sistemas
+#
+# This file is part of Merengue.
+#
+# Merengue is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Merengue is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Merengue.  If not, see <http://www.gnu.org/licenses/>.
+
+from merengue.pluggable import Plugin
+
+from plugins.contentmenu.admin import ContentGroupAdmin
+from plugins.contentmenu.models import ContentGroup
+
+
+class PluginConfig(Plugin):
+    name = 'Content menu'
+    description = 'Plugin to associate some contents between them'
+    version = '0.0.1a'
+
+    @classmethod
+    def get_model_admins(cls):
+        return [(ContentGroup, ContentGroupAdmin)]
---
+++ merengueproj/plugins/contentmenu/models.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2010 by Yaco Sistemas
+#
+# This file is part of Merengue.
+#
+# Merengue is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Merengue is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Merengue.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from merengue.base.models import BaseContent
+
+
+class ContentGroup(models.Model):
+
+    name = models.CharField(
+        verbose_name=_('identifier name of this content group'),
+        max_length=200)
+    contents = models.ManyToManyField(
+        BaseContent, verbose_name=_('contents related to the group'))
+
+    def __unicode__(self):
+        return self.name
