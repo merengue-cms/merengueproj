412cea188c17e528a41ef3466553777d0379c90c
Pablo Recio <pablo@recio.me>
2011-01-17 11:19:06 +0000

    Fixed pep8 issue. See #1
    git-svn-id: https://svn.merengueproject.org/trunk@2960 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/link/views.py
+++ merengueproj/plugins/link/views.py
@@ -27,7 +27,7 @@ LINK_COLLECTION_SLUG = 'links'
 
 
 def link_index(request, extra_context=None):
-    link_collection= Collection.objects.get(slug=LINK_COLLECTION_SLUG)
+    link_collection = Collection.objects.get(slug=LINK_COLLECTION_SLUG)
     extra_context = extra_context or {}
     return content_view(request, link_collection, extra_context=extra_context)
 
