a8d2ae2358fef92224fc9c3c5d427645f3b8f929
msaelices <msaelices@yaco.es>
2011-01-19 11:55:40 +0000

    Improved validation in subscription forms, in subscription plugin. Fixes #1234.
    git-svn-id: https://svn.merengueproject.org/trunk@3094 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/subscription/forms.py
+++ merengueproj/plugins/subscription/forms.py
@@ -8,8 +8,8 @@ class SubscribableAdminForm(forms.ModelForm):
 
     def clean(self):
         cleaned_data = self.cleaned_data
-        start_date = cleaned_data['start_date']
-        end_date = cleaned_data['end_date']
-        if end_date < start_date:
+        start_date = cleaned_data.get('start_date', None)
+        end_date = cleaned_data.get('end_date', None)
+        if start_date and end_date and end_date < start_date:
             raise forms.ValidationError(_('End date should be greater than start date.'))
         return super(SubscribableAdminForm, self).clean()
