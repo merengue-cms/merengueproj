8c42814b9df6b37bd4240dcddfce4a69e49267ef
msaelices <msaelices@yaco.es>
2011-01-22 02:58:33 +0000

    Preparing 0.6.0-rc1 release. See #1166.
    git-svn-id: https://svn.merengueproject.org/branches/0.6.X@3185 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/__init__.py
+++ merengueproj/merengue/__init__.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Merengue.  If not, see <http://www.gnu.org/licenses/>.
 
-VERSION = (0, 6, 0, 'beta', 1)
+VERSION = (0, 6, 0, 'rc', 1)
 
 
 def get_version():
