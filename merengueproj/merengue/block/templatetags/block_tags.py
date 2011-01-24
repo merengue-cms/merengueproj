d6295adb6042b79121a2ebb7eee5836bf2d8cd80
Pablo Recio <pablo@recio.me>
2011-01-24 19:00:07 +0000

    Improved filters of registerd blocks. Checks also the BlockContentRelation overwriten
    See #1310
    
    
    git-svn-id: https://svn.merengueproject.org/trunk@3225 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/block/templatetags/block_tags.py
+++ merengueproj/merengue/block/templatetags/block_tags.py
@@ -16,6 +16,7 @@
 # along with Merengue.  If not, see <http://www.gnu.org/licenses/>.
 
 from django import template
+from django.db.models import Q
 from django.conf import settings
 
 from merengue.base.models import BaseContent
@@ -58,7 +59,16 @@ def _render_blocks_queryset(blocks, request, obj, place, block_type, context):
 def _render_blocks(request, obj, place, block_type, context):
     rendered_blocks = []
     content = context.get('content')
-    registered_blocks = RegisteredBlock.objects.actives(ordered=True)
+    content_related_blocks = BlockContentRelation.objects.filter(
+        content=content)
+    filter_filters = Q(placed_at=place) | Q(placed_at='all')
+    exclude_filters = Q(blockcontentrelation__in=content_related_blocks.filter(
+            placed_at=place, overwrite_if_place=True)) | Q(
+        blockcontentrelation__in=content_related_blocks.filter(
+                    overwrite_allways=True),
+        )
+    registered_blocks = RegisteredBlock.objects.exclude(exclude_filters).filter(
+        filter_filters).actives(ordered=True)
     static_content_blocks = RegisteredBlock.objects.filter(
         blockcontentrelation__in=BlockContentRelation.objects.filter(
             content=content, placed_at=place))
