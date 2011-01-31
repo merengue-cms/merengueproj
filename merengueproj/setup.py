67f6e9a0ba6caccf59f45bc9ef214be658509783
msaelices <msaelices@yaco.es>
2011-01-31 01:41:29 +0000

    Preparing 0.6.1 release. See #1352. no-pep8.
    git-svn-id: https://svn.merengueproject.org/branches/0.6.X@3327 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/setup.py
+++ merengueproj/setup.py
@@ -63,7 +63,7 @@ for dirpath, dirnames, filenames in os.walk('.'):
 
 setup(
     name = "merengue",
-    version = "0.6.0-final",
+    version = "0.6.1",
     install_requires = parse_requirements('requirements.txt'),
     dependency_links = parse_dependency_links('requirements.txt'),
     author = "Yaco Sistemas",
