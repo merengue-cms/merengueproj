2a881e806125ba96c6f174359bd8f078b6f41c58
Rosa Durante <me@rosadurante.com>
2011-02-02 16:57:07 +0000

    see #1311. Fixed mistakes
    git-svn-id: https://svn.merengueproject.org/trunk@3385 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/tests/selenium/generic/run_suites.py
+++ merengueproj/tests/selenium/generic/run_suites.py
@@ -17,6 +17,7 @@
 # along with Merengue.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
+import sys
 
 
 def run_all_suite():
@@ -25,8 +26,18 @@ def run_all_suite():
                           and 'suite.html' in os.listdir(i)]
     if 'selenium-server.jar' in os.listdir('.'):
         for directory in directory_list:
-            os.system('java -jar selenium-server.jar -htmlSuite "*firefox" "" %s %s' \
-                          % (os.path.abspath(directory),
+            print '------------------------ SELENIUM TEST CASE ---------------------------'
+            print 'java -jar selenium-server.jar -htmlSuite "*firefox" "%s" "%s" "%s"' \
+                          % (sys.argv[1],
+                             os.path.join(os.path.abspath(directory), 'suite.html'),
+                             os.path.join(os.path.join('.'), 'results', directory + '.html'))
+
+            os.system('java -jar selenium-server.jar -htmlSuite "*firefox" "%s" "%s" "%s"' \
+                          % (sys.argv[1],
+                             os.path.join(os.path.abspath(directory), 'suite.html'),
                              os.path.join(os.path.join('.'), 'results', directory + '.html')))
     else:
         print 'ERROR: File selenium-server.jar can not be found.'
+
+if __name__ == '__main__':
+    run_all_suite()
