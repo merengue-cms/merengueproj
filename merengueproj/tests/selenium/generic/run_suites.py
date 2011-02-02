c1f3c5d25978b68a152b4801d62bbf59cc509c49
Rosa Durante <me@rosadurante.com>
2011-02-02 16:03:57 +0000

    see #1311. Delete url base of selenium test. Add script to run all suits and save results on results directory.
    git-svn-id: https://svn.merengueproject.org/trunk@3383 5c12d508-d76f-0410-91c7-e3e856e0eb0f

---
+++ merengueproj/tests/selenium/generic/run_suites.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+# Copyright (c) 2010 by Yaco Sistemas
+#
+# Merengue is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Merengue is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Merengue.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+
+def run_all_suite():
+    directory_list = [i for i in os.listdir('.') if os.path.isdir(i) \
+                          and not i.startswith('.') \
+                          and 'suite.html' in os.listdir(i)]
+    if 'selenium-server.jar' in os.listdir('.'):
+        for directory in directory_list:
+            os.system('java -jar selenium-server.jar -htmlSuite "*firefox" "" %s %s' \
+                          % (os.path.abspath(directory),
+                             os.path.join(os.path.join('.'), 'results', directory + '.html')))
+    else:
+        print 'ERROR: File selenium-server.jar can not be found.'
