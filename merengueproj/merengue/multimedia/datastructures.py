e7b9b08055d273a1497dbd4b5d687646d128d59e
msaelices <msaelices@yaco.es>
2011-02-08 01:10:11 +0000

    * Improved the duplicated checking in media assets.  * Add some new assets to media placeholders.  * Compressing other assets, like mootools. See #1418.
    git-svn-id: https://svn.merengueproject.org/trunk@3528 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/multimedia/datastructures.py
+++ merengueproj/merengue/multimedia/datastructures.py
@@ -1,6 +1,22 @@
 from BeautifulSoup import BeautifulSoup
 
 
+def is_included(node, media_list):
+    if node.name == 'script':
+        attrs_to_compare = ('src', 'id', )
+    else:  # css
+        attrs_to_compare = ('href', 'id', )
+    for media in media_list:
+        if node.name != media.name:
+            continue
+        for attr in attrs_to_compare:
+            if node.get(attr) != media.get(attr):
+                break
+            # all attributes are equal
+            return True
+    return False
+
+
 class MediaList(list):
 
     def __init__(self, namespace):
@@ -15,10 +31,9 @@ class MediaList(list):
         soup = BeautifulSoup('\n'.join(self))
         media_assets = []
         for node in soup.findAll():  # remove duplicated media assets
-            media = unicode(node)
-            if media not in media_assets:
-                media_assets.append(media)
-        return '\n'.join(media_assets)
+            if not is_included(node, media_assets):
+                media_assets.append(node)
+        return '\n'.join([unicode(node) for node in media_assets])
 
 
 class MediaDictionary(dict):
