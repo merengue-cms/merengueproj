7f1a6ce97f265d484fcbe7606d70e488e46272b5
Emilio A. Sanchez Lopez <esanchez@cymoril>
2011-02-14 13:02:59 +0000

    Check for valid lookups over selected field. Fixes #1355
    git-svn-id: https://svn.merengueproject.org/trunk@3652 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/collection/forms.py
+++ merengueproj/merengue/collection/forms.py
@@ -16,6 +16,7 @@
 # along with Merengue.  If not, see <http://www.gnu.org/licenses/>.
 
 from django import forms
+from django.contrib.contenttypes.models import ContentType
 from django.utils.importlib import import_module
 
 
@@ -27,12 +28,30 @@ class CollectionFilterForm(forms.ModelForm):
     ISNULL_VALID_CHOICES = ('true', 'false', )
 
     def clean(self):
-#        @FIXME: Hardcoded string
         cdata = self.cleaned_data
         if cdata.get('filter_operator') == 'isnull' and \
             cdata.get('filter_value') not in self.ISNULL_VALID_CHOICES:
             raise forms.ValidationError("Value should be 'true' or 'false'")
-
+        if not self.instance:
+            return self.cleaned_data
+        collection = cdata.get('collection', None)
+        if not collection:
+            return self.cleaned_data
+        if collection.pk:
+            content_types = collection.content_types.all()
+        else:
+            content_types = ContentType.objects.filter(id__in=self.data.getlist('content_types'))
+        if not content_types:
+            return self.cleaned_data
+        try:
+            model = content_types[0].model_class()
+            query = model.objects.all()
+            self.instance.filter_field = cdata.get('filter_field')
+            self.instance.filter_operator = cdata.get('filter_operator')
+            self.instance.filter_value = cdata.get('filter_value')
+            self.instance.filter_query(query)
+        except Exception, e:
+                raise forms.ValidationError(e)
         return self.cleaned_data
 
 
