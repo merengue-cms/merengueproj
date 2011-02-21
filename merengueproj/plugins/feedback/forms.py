24883c8111c5efdf8454bed45718957a5b87e276
Jacobo Tarragón <jacobo.tarragon@gmail.com>
2011-02-21 14:18:53 +0000

    enable captcha for feedback plugin. Fixes #1534.
    git-svn-id: https://svn.merengueproject.org/trunk@3815 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/feedback/forms.py
+++ merengueproj/plugins/feedback/forms.py
@@ -19,7 +19,7 @@ from merengue.base.forms import BaseModelForm
 
 from threadedcomments.models import FreeThreadedComment
 from threadedcomments.forms import FreeThreadedCommentForm
-#from captcha.fields import CaptchaField
+from captcha.fields import CaptchaField
 
 
 class CaptchaFreeThreadedCommentForm(BaseModelForm, FreeThreadedCommentForm):
@@ -30,7 +30,7 @@ class CaptchaFreeThreadedCommentForm(BaseModelForm, FreeThreadedCommentForm):
 
     def __init__(self, user, *args, **kwargs):
         super(CaptchaFreeThreadedCommentForm, self).__init__(*args, **kwargs)
-#        if user.is_anonymous():
-#            captcha_field = CaptchaField()
-#            self.fields['captcha'] = captcha_field
-#            self.declared_fields['captcha'] = captcha_field
+        if user.is_anonymous():
+            captcha_field = CaptchaField()
+            self.fields['captcha'] = captcha_field
+            self.declared_fields['captcha'] = captcha_field
