49bd0a3b18560f0325c3d1a7bb58759a94369181
msaelices <msaelices@yaco.es>
2011-02-21 22:06:23 +0000

    See #1458. Some things done:  * Use optparse, better than parsing manually sys.argv.  * Define run_suites.py file as executable.
    git-svn-id: https://svn.merengueproject.org/trunk@3822 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/tests/selenium/generic/run_suites.py
+++ merengueproj/tests/selenium/generic/run_suites.py
@@ -17,41 +17,47 @@
 # along with Merengue.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
-import sys
 import shutil
+from optparse import OptionParser
 
 
 def run_all_suite():
+    usage = "usage: %prog [options] url_base"
+    parser = OptionParser(usage)
+    parser.add_option("-q", "--quiet",
+                  action="store_false", dest="verbose", default=True,
+                  help="don't print status messages to stdout")
+    (options, args) = parser.parse_args()
+
     directory_list = [i for i in os.listdir('.') if os.path.isdir(i) \
                           and not i.startswith('.') \
                           and 'suite.html' in os.listdir(i)]
-    if len(sys.argv) == 1:
-        print """
-ERROR: Can not found base url.
-USE: python run_suites.py 'url base for selenium test, as http://localhost:8000/'
-"""
+    if len(args) != 1:
+        parser.error("You need to define a base URL to launch the test "
+                     "(i.e. http://localhost:8000/)""")
+    pwd = os.path.dirname(os.path.abspath(__file__))
+    extensions_file = os.path.join(os.path.abspath('..'), 'extensions', 'user-extensions.js')
+    selenium_file = os.path.join(pwd, 'selenium-server.jar')
+    variables_file = os.path.join(pwd, 'variables.html')
+    if os.path.exists(extensions_file) and os.path.exists(selenium_file):
+        for directory in directory_list:
+            os.chdir(pwd)
+            directory_path = os.path.join(pwd, directory)
+            suite_file = os.path.join(directory_path, 'suite.html')
+            results_file = os.path.join(pwd, 'results', directory + '.html')
+            variables_copy = os.path.join(directory_path, 'variables.html')
+            shutil.copy(variables_file, variables_copy)
+            if options.verbose:
+                print 'Launching Selenium RC in %s test suite...' % suite_file
+            os.system('java -jar %s -htmlSuite "*firefox" "%s" "%s" "%s" -userExtensions "%s"' \
+                            % (selenium_file,
+                            args[0],
+                            suite_file,
+                            results_file,
+                            extensions_file))
+            os.remove(variables_copy)
     else:
-        pwd = os.path.dirname(os.path.abspath(__file__))
-        extensions_file = os.path.join(os.path.abspath('..'), 'extensions', 'user-extensions.js')
-        selenium_file = os.path.join(pwd, 'selenium-server.jar')
-        variables_file = os.path.join(pwd, 'variables.html')
-        if os.path.exists(extensions_file) and os.path.exists(selenium_file):
-            for directory in directory_list:
-                os.chdir(pwd)
-                directory_path = os.path.join(pwd, directory)
-                suite_file = os.path.join(directory_path, 'suite.html')
-                results_file = os.path.join(pwd, 'results', directory + '.html')
-                variables_copy = os.path.join(directory_path, 'variables.html')
-                shutil.copy(variables_file, variables_copy)
-                os.system('java -jar %s -htmlSuite "*firefox" "%s" "%s" "%s" -userExtensions "%s"' \
-                              % (selenium_file,
-                                 sys.argv[1],
-                                 suite_file,
-                                 results_file,
-                                 extensions_file))
-                os.remove(variables_copy)
-        else:
-            print 'ERROR: File selenium-server.jar/user-extensions.js can not be found.'
+        print 'ERROR: File selenium-server.jar/user-extensions.js can not be found.'
 
 if __name__ == '__main__':
     run_all_suite()
