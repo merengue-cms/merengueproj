29a8db57436d55e6ab18604cb70f065cd22e4308
Emilio A. Sanchez Lopez <esanchez@cymoril>
2011-02-22 08:01:59 +0000

    Revert public link of a thread. Fixes #1487
    git-svn-id: https://svn.merengueproject.org/trunk@3835 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/forum/models.py
+++ merengueproj/plugins/forum/models.py
@@ -64,7 +64,8 @@ class Thread(BaseContent):
     def get_main_section(self):
         return self.forum.get_main_section()
 
-    def _public_link_without_section(self):
+    @permalink
+    def public_link(self):
         return ('thread_view', [self.forum.slug, self.slug])
 
     def save(self, *args, **kwargs):
