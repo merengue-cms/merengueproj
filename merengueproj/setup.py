4d3d4f28593836072cff459f20c95675b151b769
msaelices <msaelices@yaco.es>
2011-02-24 18:29:33 +0000

    Preparing new version, which fixes some problems like bad django-inlinetrans requirement. See #1576.
    git-svn-id: https://svn.merengueproject.org/trunk@3915 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/setup.py
+++ merengueproj/setup.py
@@ -63,7 +63,7 @@ for dirpath, dirnames, filenames in os.walk('.'):
 
 setup(
     name="merengue",
-    version="0.7.0-beta3",
+    version="0.7.0-beta4",
     install_requires=parse_requirements('requirements.txt'),
     dependency_links=parse_dependency_links('requirements.txt'),
     author="Yaco Sistemas",
