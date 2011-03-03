40059724a0513d47bbbd610bb4be5e4005fa3610
msaelices <msaelices@yaco.es>
2011-03-03 10:47:54 +0000

    Check for existence of forum models before doing first migration. Fixes #1601.
    git-svn-id: https://svn.merengueproject.org/trunk@3963 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/forum/migrations/0001_initial.py
+++ merengueproj/plugins/forum/migrations/0001_initial.py
@@ -2,13 +2,15 @@
 import datetime
 from south.db import db
 from south.v2 import SchemaMigration
-from merengue.base.utils import south_trans_data, add_south_trans_fields
+from merengue.base.utils import south_trans_data, add_south_trans_fields, table_exists
 
 
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding model 'ForumCategory'
+        if table_exists('forum_forumcategory'):
+            return  # already migrated
         forumcategory_data = (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('slug', self.gf('django.db.models.fields.SlugField')(max_length=200, db_index=True)),
