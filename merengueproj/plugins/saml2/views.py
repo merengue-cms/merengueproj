dc509b68163cf8a022b080d1e35fdae7c4b01164
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2011-03-24 18:37:13 +0000

    Custom logout service view for Merengue that send a message to the user indicating a successfull logout. See #1442
    git-svn-id: https://svn.merengueproject.org/trunk@4202 5c12d508-d76f-0410-91c7-e3e856e0eb0f

---
+++ merengueproj/plugins/saml2/views.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2011 by Yaco Sistemas
+#
+# This file is part of Merengue.
+#
+# Merengue is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Merengue is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Merengue.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.utils.translation import gettext as _
+
+from djangosaml2.views import logout_service
+
+from cmsutils.log import send_info
+
+from plugins.saml2.saml_config_loader import merengue_config_loader
+
+
+def merengue_logout_service(request):
+    """Do the logout and then send a message to the user"""
+    response = logout_service(request, config_loader=merengue_config_loader,
+                              next_page='/')
+    if 'SAMLResponse' in request.GET:  # we started the logout
+        send_info(request, _('Thank you for your visit'))
+    return response
