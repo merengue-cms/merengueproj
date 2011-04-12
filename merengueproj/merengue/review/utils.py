7e95ff53d23aed847f9699e942e29c1eda5843b8
msaelices <msaelices@yaco.es>
2011-04-12 08:30:33 +0000

    See #1721. Fixes #1773. Merged revisions 4295 via svnmerge from https://svn.merengueproject.org/trunk
    git-svn-id: https://svn.merengueproject.org/branches/0.7.X@4337 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/review/utils.py
+++ merengueproj/merengue/review/utils.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.core.mail import send_mail
-from django.utils.html import escape
+from django.contrib.sites.models import Site
+from django.core.mail import EmailMessage
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext
 from django.template.loader import render_to_string
@@ -16,13 +16,17 @@ def send_mail_content_as_pending(obj, review_task, users=None,
     """
     if not users:
         users = User.objects.all()
-    subject = ugettext(u'%s has been set as pending') % getattr(obj, get_real_fieldname('name'))
+    subject = ugettext(u'%s has been set as pending') % getattr(obj, get_real_fieldname('name'),
+                                                                obj.name)
     body = render_to_string(template, {
             'content': obj,
-            'task_admin_url': reverse('admin:review_reviewtask_change',
-                                      args=(review_task.pk,))})
+            'task_admin_url': 'http://%s%s' % (Site.objects.get(id=settings.SITE_ID).domain,
+                                               reverse('admin:review_reviewtask_change',
+                                                       args=(review_task.pk,)))})
     from_mail = settings.EMAIL_HOST_USER
     recipers = list(set([user.email for user in users if user.email]))
 
     for reciper in recipers:
-        send_mail(subject, escape(body), from_mail, [reciper], fail_silently=False)
+        email = EmailMessage(subject, body, from_mail, [reciper])
+        email.content_subtype = "html"
+        email.send()
