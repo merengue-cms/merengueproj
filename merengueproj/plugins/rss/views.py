4f2b75bad16ebc07d425ecdc51eab7b8d5738e36
Pablo Recio <pablo@recio.me>
2011-05-10 09:35:16 +0000

    Added new function on RSS plugins that generate the feed given a queryset.
    If none is given, it generates the default queryset.
    
    See #1884
    
    
    git-svn-id: https://svn.merengueproject.org/trunk@4593 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/rss/views.py
+++ merengueproj/plugins/rss/views.py
@@ -15,66 +15,11 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Merengue.  If not, see <http://www.gnu.org/licenses/>.
 
-from datetime import datetime
-
-from django.db.models import Q
-from django.contrib.sites.models import Site
 from django.http import HttpResponse
-from django.utils import feedgenerator
-from django.template.loader import render_to_string
 
-from merengue.base.models import BaseContent
-from merengue.registry.items import ALL_TYPES
-
-from merengue.pluggable.utils import get_plugin
+from plugins.rss.utils import generate_feed_from_queryset
 
 
 def rss_views(request):
-    plugin = get_plugin('rss')
-    contenttypes = plugin.get_config().get(
-        'contenttypes', []).get_value()
-
-    query = Q()
-    if not contenttypes or ALL_TYPES in contenttypes:
-        results = BaseContent.objects.filter(
-            status='published').order_by('modification_date')[::-1]
-    else:
-        classnames = [x for x in contenttypes]
-        for classname in classnames:
-            query = query | Q(class_name=classname.lower())
-        results = BaseContent.objects.filter(status='published').filter(
-            query).order_by('modification_date')[::-1]
-
-    portal_title = plugin.get_config().get(
-        'portal', '').get_value()
-    f = feedgenerator.Rss201rev2Feed(
-        title=portal_title,
-        link=render_to_string('rss/link.html'),
-        description=render_to_string('rss/description.html'),
-        language=render_to_string('rss/language.html'),
-        author_name=render_to_string('rss/author_name.html'),
-        feed_url=render_to_string('rss/feed_url.html'),
-    )
-    limit = plugin.get_config().get('limit', None)
-    results = results[:int(limit.get_value())]
-    link_prefix = 'http://%s' % Site.objects.all()[0].domain
-    for item in results:
-        if 'modification_date' in item.__dict__:
-            item_date = item.modification_date
-        else:
-            item_date = datetime.now()
-        templates = {'title': ['rss/%s/title.html' % item.class_name,
-                               'rss/items/title.html'],
-                     'description': ['rss/%s/description.html' % item.class_name,
-                               'rss/items/description.html'],
-                    }
-        f.add_item(
-            title=render_to_string(templates['title'], {'item': item}),
-            link=u'%s%s' % (link_prefix, item.public_link()),
-            pubdate=item_date,
-            description=render_to_string(templates['description'],
-                                         {'item': item}),
-        )
-
-    return HttpResponse(f.writeString('UTF-8'),
-                        mimetype="application/atom+xml")
+    feed = generate_feed_from_queryset()
+    return HttpResponse(feed, mimetype="application/atom+xml")
