c66dc1fa1099f85430210747f09350d8c44a0b3a
Pablo Martín <goinnn@gmail.com>
2011-05-18 11:25:43 +0000

    See #1 #1856 Fix a little error
    git-svn-id: https://svn.merengueproject.org/trunk@4635 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/smartsearch/utils.py
+++ merengueproj/plugins/smartsearch/utils.py
@@ -6,11 +6,12 @@ from autoreports.utils import get_field_from_model, get_adaptor
 def get_fields(search):
     model = search.content_type.model_class()
     fields_form_filter = SortedDict({})
+    fields_form_display = SortedDict({})
 
     for field_name, opts in search.options.items():
         model_field, field = get_field_from_model(model, field_name)
         adaptor = get_adaptor(field)(model_field, field, field_name)
         adaptor.get_field_form(opts, default=True,
                                 fields_form_filter=fields_form_filter,
-                                fields_form_display=None)
+                                fields_form_display=fields_form_display)
     return fields_form_filter
