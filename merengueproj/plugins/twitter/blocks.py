f0108df593449406584d04ea10b8ce0e3670484c
Rosa Durante <me@rosadurante.com>
2011-05-20 10:40:53 +0000

    see #1907. Add plugin twitter
    git-svn-id: https://svn.merengueproject.org/trunk@4657 5c12d508-d76f-0410-91c7-e3e856e0eb0f

---
+++ merengueproj/plugins/twitter/blocks.py
@@ -0,0 +1,118 @@
+# Copyright (c) 2010 by Yaco Sistemas
+#
+# This file is part of Merengue.
+#
+# Merengue is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Merengue is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Merengue.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.utils.translation import ugettext_lazy as _, ugettext
+
+from merengue.block.blocks import Block
+from merengue.registry import params
+from plugins.twitter.utils import twitter_api
+
+
+class TimelineProvider:
+    template_name = ''
+    block_title = ''
+
+    def get_tweets(self):
+        raise NotImplementedError('Abstact method')
+
+    def get_link(self):
+        raise NotImplementedError('Abstact method')
+
+    def render(self, request, place, context, *args, **kwargs):
+        tweets_list = self.get_tweets()
+        link_twitter = self.get_link()
+        return self.render_block(request, template_name=self.template_name,
+                                 block_title=self.block_title,
+                                 context={'tweets': {'error': tweets_list[1],
+                                                     'list': tweets_list[0]},
+                                          'link': {'title': link_twitter[0],
+                                                   'http': link_twitter[1]}})
+
+
+class UserTimelineBlock(TimelineProvider, Block):
+    name = 'usertimeline'
+    default_place = 'rightsidebar'
+    verbose_name = _('Timeline of Twitter User')
+    help_text = _('Block to show a timeline of Twitter for an user.')
+    template_name = 'twitter/timeline_block.html'
+
+    config_params = [params.PositiveInteger(name='limit',
+                                            label=ugettext('Limit for twitter block'),
+                                            default=3),
+                     params.Single(name='user',
+                                   label=ugettext('Tweets of this user'),
+                                   default='merengueproject'),
+                     ]
+
+    def get_tweets(self):
+
+        limit = self.get_config().get('limit', []).get_value()
+        user = self.get_config().get('user', []).get_value()
+
+        self.block_title = user + "'s tweets!"
+
+        api = twitter_api(limit, user)
+        (user_tweets, error) = api.get_user_tweets()
+
+        render_tweets = []
+        for tweet in user_tweets:
+            render_tweets.append(api.convert_tweet_html(tweet))
+
+        return (render_tweets, error)
+
+    def get_link(self):
+        error = self.get_tweets()
+        if "doesn't exists" in error[1]:
+            return ('Twitter Mainpage', 'http://www.twitter.com/')
+        else:
+            user = self.get_config().get('user', []).get_value()
+            return ('Follow ' + user + ' on Twitter!', 'http://www.twitter.com/#!/' + user + '/')
+
+
+class HashtagTimelineBlock(TimelineProvider, Block):
+    name = 'hashtagtimeline'
+    default_place = 'rightsidebar'
+    verbose_name = _('Timeline of Tag in Twitter ')
+    help_text = _('Block to show a timeline of Twitter for a hashtag.')
+    template_name = 'twitter/timeline_block.html'
+
+    config_params = [params.PositiveInteger(name='limit',
+                                            label=ugettext('Limit for twitter block'),
+                                            default=3),
+                     params.Single(name='hashtag',
+                                   label=ugettext('Tweets of a hashtag'),
+                                   default='#merengueproject'),
+                     ]
+
+    def get_tweets(self):
+        limit = self.get_config().get('limit', []).get_value()
+        hashtag = self.get_config().get('hashtag', []).get_value()
+
+        self.block_title = hashtag
+
+        api = twitter_api(limit, hashtag)
+        (hashtag_tweets, no_tweet_found) = api.get_hashtags_tweets()
+
+        render_tweets = []
+        for tweet in hashtag_tweets:
+            render_tweets.append(api.convert_tweet_html(tweet))
+
+        return (render_tweets, no_tweet_found)
+
+    def get_link(self):
+        hashtag = self.get_config().get('hashtag', []).get_value()
+        return ('Search ' + hashtag + ' on Twitter!', 'http://www.twitter.com/#!/search/' + hashtag + '/')
---
+++ merengueproj/plugins/twitter/utils.py
@@ -0,0 +1,89 @@
+# Copyright (c) 2010 by Yaco Sistemas
+#
+# This file is part of Merengue.
+#
+# Merengue is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Merengue is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Merengue.  If not, see <http://www.gnu.org/licenses/>.
+
+import re
+import twitter
+
+
+class twitter_api():
+    def __init__(self, limit, value):
+        self.api = twitter.Api()
+        self.limit = limit
+        self.value = value
+
+    def get_user_tweets(self):
+        try:
+            if self.api.GetUser(self.value).protected:
+                return ([], u'This user has protected his tweets')
+            else:
+                tweets = self.api.GetUserTimeline(screen_name=self.value,
+                                                  count=self.limit,
+                                                  include_rts=True)
+                if not tweets:
+                    return ([], u"This user hasn't tweets yet")
+
+                else:
+                    return ([i.text for i in tweets], u'Ok')
+
+        except twitter.TwitterError:
+            return ([], u"This user doesn't exists")
+
+    def get_hashtags_tweets(self):
+        tweets = self.api.GetSearch(term=self.value,
+                                   lang='es',
+                                   per_page=self.limit,
+                                   show_user=True)
+
+        return ([i.text for i in tweets], u"There aren't results for the hashtag defined")
+
+    def convert_tweet_html(self, tweet):
+        # comprobar que exista un enlace:
+        if 'http' in tweet:
+            pattern_url = re.compile("https?://[a-zA-Z0-9./-_]+")
+            url = pattern_url.search(tweet)
+            while url is not None:
+                rpl = "<a href='" + url.group() + "'>" + url.group() + "</a>"
+                pos = int(url.end()) + len(rpl) - len(url.group())
+
+                tweet = tweet.replace(url.group(), rpl)
+                url = pattern_url.search(tweet, pos)
+
+        # comprobar si existe '#'
+        if '#' in tweet:
+            pattern_tag = re.compile("#[a-zA-Z0-9]+")
+            tag = pattern_tag.search(tweet)
+            while tag is not None:
+                rpl = "<a href='http://twitter.com/#!/search/" + tag.group()[1:] + \
+                    "'>" + tag.group() + "</a>"
+                pos = int(tag.end()) + len(rpl) - len(tag.group())
+
+                tweet = tweet.replace(tag.group(), rpl)
+                tag = pattern_tag.search(tweet, pos)
+
+        # comprobar si existe '@'
+        if '@' in tweet:
+            pattern_user = re.compile("@[a-zA-Z0-9_]+")
+            user = pattern_user.search(tweet)
+            while user is not None:
+                rpl = "<a href='http://twitter.com/#!/" + user.group()[1:] + \
+                    "'>" + user.group() + "</a>"
+                pos = int(user.end()) + len(rpl) - len(user.group())
+
+                tweet = tweet.replace(user.group(), rpl)
+                user = pattern_user.search(tweet, pos)
+
+        return tweet
