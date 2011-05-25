0e3c48252bb70bc1abfae46269ab28683043a95f
Rosa Durante <me@rosadurante.com>
2011-05-25 14:37:15 +0000

    see #1907. Add the event to refresh every minute and a half the hashtag block and every five minutes the usertimeline block
    git-svn-id: https://svn.merengueproject.org/trunk@4707 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/twitter/media/js/refresh.js
+++ merengueproj/plugins/twitter/media/js/refresh.js
@@ -1,29 +1,36 @@
+// # Copyright (c) 2010 by Yaco Sistemas 
+// #
+// # This file is part of Merengue. 
+// #
+// # Merengue is free software: you can redistribute it and/or modify 
+// # it under the terms of the GNU Lesser General Public License as published by
+// # the Free Software Foundation, either version 3 of the License, or 
+// # (at your option) any later version.
+// #
+// # Merengue is distributed in the hope that it will be useful,
+// # but WITHOUT ANY WARRANTY; without even the implied warranty of
+// # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// # GNU Lesser General Public License for more details.
+// #
+// # You should have received a copy of the GNU Lesser General Public License
+// # along with Merengue.  If not, see <http://www.gnu.org/licenses/>.
 
-//$(document).ready(function (data) {
-//	block_id = $("#usertimeline input").val();
-//	alert(block_id);
-//}(jQuery));
 
-
-//$(document).ready(function (){
 function refresh_usertimeline() {
-	$(".tweet img").bind("mouseover", function() {
-			block_id_usertimeline = $("#usertimeline input").val();
-			alert(block_id_usertimeline);
-			$.get("twitter/ajax/get_user_tweets/" + block_id_usertimeline, 
-				  function(data){
-					  alert(data);
-					  $('#usertimeline div.tweetlist').html(data);
-				  });
-		});
+	block_id_usertimeline = jQuery("#usertimeline input").val();
+	jQuery.get("twitter/ajax/get_user_tweets/" + block_id_usertimeline, 
+			   function(data){
+				   jQuery('#usertimeline div#tweetlist').html(data);
+			   });
 }(jQuery);
 
 function refresh_hashtagtimeline() {
-$(".tweet img").bind("mouseover", function() {
-	block_id_hashtagtimeline = $("#hashtagtimeline input").val();
-	$.get("twitter/ajax/get_hashtag_tweets/" + block_id_hashtagtimeline,
-		  function(data){
-			  $('#hashtagtimeline div.tweetlist').html(data);
-		  });
-	});
+	block_id_hashtagtimeline = jQuery("#hashtagtimeline input").val();
+	jQuery.get("twitter/ajax/get_hashtag_tweets/" + block_id_hashtagtimeline,
+			   function(data){
+				   jQuery('#hashtagtimeline div#tweetlist').html(data);
+			   });
 }(jQuery);
+
+setInterval("refresh_usertimeline()", 1000 * 60 * 5);
+setInterval("refresh_hashtagtimeline()", 1000 * 90);
