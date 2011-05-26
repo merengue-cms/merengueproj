598bda19d7e9bbb494b0a20f84880954ec27c370
msaelices <msaelices@yaco.es>
2011-05-26 15:14:42 +0000

    I've forgotten to add this file. Fixes #1928.
    git-svn-id: https://svn.merengueproject.org/trunk@4720 5c12d508-d76f-0410-91c7-e3e856e0eb0f

---
+++ merengueproj/merengue/base/lookups.py
@@ -0,0 +1,48 @@
+# Copyright (c) 2010 by Yaco Sistemas
+#
+# This file is part of Merengue.
+#
+# Merengue is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Merengue is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Merengue.  If not, see <http://www.gnu.org/licenses/>.
+
+from django.db.models import Q
+
+from transmeta import get_real_fieldname_in_each_language
+from merengue.base.models import BaseContent
+
+
+class ContentLookup(object):
+
+    def get_query(self, q, request):
+        """ return a query set. you also have access to request.user if needed """
+        name_fields = get_real_fieldname_in_each_language('name')
+        query = Q()  # query without filters
+        for field_name in name_fields:
+            query |= Q(**{'%s__contains' % field_name: q})
+        autocompleted = BaseContent.objects.filter(query).visible_by_user(request.user)
+        return autocompleted
+
+    def format_item(self, content):
+        """ simple display of an object when it is displayed in the list of selected objects """
+        content = content.get_real_instance()
+        return u'%s (%s)' % (content, content._meta.verbose_name)
+
+    def format_result(self, content):
+        """ a more verbose display, used in the search results display.  may contain html and multi-lines """
+        return self.format_item(content)
+
+    def get_objects(self, ids):
+        """ given a list of ids, return the objects ordered as you would like them on the admin page.
+            this is for displaying the currently selected items (in the case of a ManyToMany field)
+        """
+        return BaseContent.objects.filter(pk__in=ids)
