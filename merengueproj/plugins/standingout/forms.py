b8d985766ee230a60977b9912be10b730d05a755
Emilio A. Sanchez Lopez <esanchez@cymoril>
2011-06-08 09:33:13 +0000

    Section or content are the standing out categories that require a related content. See #1983
    git-svn-id: https://svn.merengueproject.org/trunk@4811 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/standingout/forms.py
+++ merengueproj/plugins/standingout/forms.py
@@ -25,14 +25,16 @@ class StandingOutAdminModelForm(BaseAdminModelForm):
 
     def clean(self):
         cleaned_data = super(StandingOutAdminModelForm, self).clean()
-        if cleaned_data.get('standing_out_category', None) and not cleaned_data.get('related', None):
+        category = cleaned_data.get('standing_out_category', None)
+        category_slug = category and category.slug or None
+        if (category_slug in ['section', 'content']) and not cleaned_data.get('related', None):
             related_error = self._errors.get('related', ErrorList([]))
-            related_error_new = ErrorList([_(u'If you select the option in field standing out category you have to select a option in related field')])
+            related_error_new = ErrorList([_(u'If you select "section" or "content" in field standing out category you have to select a option in related field')])
             related_error.extend(related_error_new)
             self._errors['related'] = ErrorList(related_error)
-        elif not cleaned_data.get('standing_out_category', None) and cleaned_data.get('related', None):
+        elif (category_slug not in ['section', 'content']) and cleaned_data.get('related', None):
             standing_out_category_error = self._errors.get('standing_out_category', ErrorList([]))
-            standing_out_category_error_new = ErrorList([_(u'If you select the option in field related you have to select a option in standing out category field')])
+            standing_out_category_error_new = ErrorList([_(u'If you select the option in field related you have to select "section" or "content" in standing out category field')])
             standing_out_category_error.extend(standing_out_category_error_new)
             self._errors['standing_out_category'] = ErrorList(standing_out_category_error)
         return cleaned_data
