49dd8744a82043d9a407b69fbcd99cf4054c29e3
msaelices <msaelices@yaco.es>
2011-06-17 20:16:51 +0000

    A help text and verbose name for custom portlet block. See #1.
    git-svn-id: https://svn.merengueproject.org/trunk@4964 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/plugins/customportlet/blocks.py
+++ merengueproj/plugins/customportlet/blocks.py
@@ -1,4 +1,5 @@
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext
+from django.utils.translation import ugettext_lazy as _
 
 from merengue.block.blocks import Block
 
@@ -8,11 +9,13 @@ from plugins.customportlet.models import CustomPortlet
 class CustomPortletBlock(Block):
     name = 'customportlet-block'
     default_place = 'homepage'
+    verbose_name = _('Portlet block')
+    help_text = _('Block that renders a custom portlet')
 
     def render(self, request, place, context, *args, **kwargs):
         customportlets = CustomPortlet.objects.all()
 
         return self.render_block(
             request, template_name='customportlet/block_pd.html',
-            block_title=_('Custom portlet'),
+            block_title=ugettext('Custom portlet'),
             context={'customportlets': customportlets})
