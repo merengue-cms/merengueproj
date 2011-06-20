cae4811892abb0154e2c1eba3e3c6c272a4855a8
Pablo Martín <goinnn@gmail.com>
2011-06-20 15:54:19 +0000

    Fixes #2064 Removed restrictions menu creation
    git-svn-id: https://svn.merengueproject.org/trunk@4991 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/section/formsets.py
+++ merengueproj/merengue/section/formsets.py
@@ -1,6 +1,4 @@
 from django.forms.models import BaseInlineFormSet, save_instance
-from django.forms.util import ValidationError
-from django.utils.translation import ugettext_lazy as _
 
 
 class BaseLinkInlineFormSet(BaseInlineFormSet):
@@ -16,13 +14,3 @@ class BaseLinkInlineFormSet(BaseInlineFormSet):
         if 'baselink_ptr' in form.cleaned_data and not form.cleaned_data['baselink_ptr']:
             del(form.cleaned_data['baselink_ptr'])
         return save_instance(form, new_obj, exclude=exclude, commit=commit)
-
-    def clean(self):
-        data = self.data
-        if data.get('contentlink-0-content', None) and data.get('absolutelink-0-url', None) or \
-            data.get('viewletlink-0-viewlet', None) and data.get('contentlink-0-content', None) or \
-            data.get('viewletlink-0-viewlet', None) and data.get('absolutelink-0-content', None):
-            raise ValidationError(_('Sorry you can not select two or more options simultaneously for this menu. Fulfill just one.'))
-        elif not data.get('contentlink-0-content', None) and not data.get('absolutelink-0-url', None) and \
-                not data.get('viewletlink-0-viewlet', None):
-            raise ValidationError(_('Sorry you need to select one options for this menu. Fulfill just one.'))
