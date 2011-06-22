4b226867433cf2f72da5cfc842dd441b25390e71
msaelices <msaelices@yaco.es>
2011-06-22 17:42:47 +0000

    Some unit tests for blocks. See #2089.
    git-svn-id: https://svn.merengueproject.org/trunk@5033 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/block/tests.py
+++ merengueproj/merengue/block/tests.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2010 by Yaco Sistemas
+# Copyright (c) 2010 by Yaco Sistemas <precio@yaco.es>
 #
 # This file is part of Merengue.
 #
@@ -15,28 +15,105 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Merengue.  If not, see <http://www.gnu.org/licenses/>.
 
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
+from pprint import pprint  # pyflakes:ignore
 from django.test import TestCase
 
-
-class SimpleTest(TestCase):
-
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from merengue.base.models import BaseContent
+from merengue.block.blocks import Block, ContentBlock, SectionBlock
+from merengue.block.templatetags.block_tags import _get_blocks_to_display
+from merengue.registry import register
 
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+class BaseBlock(Block):
 
->>> 1 + 1 == 2
-True
-"""}
+    def render(self, request, place, context, *args, **kwargs):
+        reg_block = self.reg_item
+        return {
+            'name': self.name,
+            'place': reg_block.placed_at,
+            'content': reg_block.content_id,
+            'order': reg_block.order,
+            'overwrite_if_place': reg_block.overwrite_if_place,
+            'overwrite_always': reg_block.overwrite_always,
+        }
+
+
+class LeftBlock(BaseBlock):
+    name = 'leftblock'
+    default_place = 'leftsidebar'
+
+
+class RightBlock(BaseBlock):
+    name = 'rightblock'
+    default_place = 'rightsidebar'
+
+
+class FooterBlock(BaseBlock):
+    name = 'footerblock'
+    default_place = 'footer'
+
+
+class FooContentBlock(BaseBlock, ContentBlock):
+    name = 'foocontentblock'
+    default_place = 'leftsidebar'
+
+
+class FooSectionBlock(BaseBlock, SectionBlock):
+    name = 'foosectionblock'
+    default_place = 'leftsidebar'
+
+
+def get_block_names(place=None, content=None):
+    return [b['name'] for b in get_rendered_blocks(place, content)]
+
+
+def get_rendered_blocks(place=None, content=None):
+    blocks = _get_blocks_to_display(place, content)
+    return [reg_block.get_registry_item().render(None, None, None) for reg_block in blocks]
+
+
+class BlockTestCase(TestCase):
+
+    def setUp(self):
+        self.maxDiff = None  # to get large diff when a test fail
+        register(LeftBlock)
+        register(RightBlock)
+        register(FooterBlock)
+        register(FooContentBlock)
+        register(FooSectionBlock)
+        self.content = BaseContent.objects.create(name_en='A content')
+        content_reg_block = register(LeftBlock)
+        content_reg_block.content = self.content
+        content_reg_block.save()
+
+    def test_rendering_places(self):
+        """Test the blocks retrieved in every place are the block Merengue should display """
+        left_blocks_rendered = [
+            {'name': 'leftblock', 'place': u'leftsidebar', 'content': None, 'order': 0, 'overwrite_if_place': True, 'overwrite_always': False},
+            {'name': 'foocontentblock', 'place': u'leftsidebar', 'content': None, 'order': 0, 'overwrite_if_place': True, 'overwrite_always': False},
+            {'name': 'foosectionblock', 'place': u'leftsidebar', 'content': None, 'order': 0, 'overwrite_if_place': True, 'overwrite_always': False},
+        ]
+        right_blocks_rendered = [
+            {'name': 'rightblock', 'place': u'rightsidebar', 'content': None, 'order': 0, 'overwrite_if_place': True, 'overwrite_always': False},
+        ]
+        footer_blocks_rendered = [
+            {'name': 'footerblock', 'place': u'footer', 'content': None, 'order': 0, 'overwrite_if_place': True, 'overwrite_always': False},
+        ]
+        left_blocks_rendered_in_content = [
+            {'name': 'leftblock', 'place': u'leftsidebar', 'content': self.content.id, 'order': 0, 'overwrite_if_place': True, 'overwrite_always': False},
+        ]
+        self.assertEqual(left_blocks_rendered, get_rendered_blocks('leftsidebar'))
+        self.assertEqual(right_blocks_rendered, get_rendered_blocks('rightsidebar'))
+        self.assertEqual(footer_blocks_rendered, get_rendered_blocks('footer'))
+        self.assertEqual(left_blocks_rendered_in_content, get_rendered_blocks('leftsidebar', self.content))
+        self.assertEqual([], get_rendered_blocks('rightsidebar', self.content))
+
+    def test_block_overwriting(self):
+        """ Test block overriding behavior """
+        content_reg_block = register(LeftBlock)
+        content_reg_block.content = self.content
+        content_reg_block.overwrite_always = True
+        content_reg_block.placed_at = 'rightsidebar'
+        content_reg_block.save()
+        # this block will be placed in the "rightsidebar" and will hide the leftsidebar blocks
+        self.assertTrue('leftblock' not in get_block_names('leftsidebar', self.content))
