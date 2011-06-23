d7f635ea3ea72eec32c1765264914d28cb2d16ee
Pablo Martín <goinnn@gmail.com>
2011-06-23 16:04:15 +0000

    Fixes #2105 Removed infinite loop
    git-svn-id: https://svn.merengueproject.org/trunk@5058 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/section/views.py
+++ merengueproj/merengue/section/views.py
@@ -96,7 +96,10 @@ def menu_section_view(request, section_slug, menu_slug):
         return render_to_response('section/menu_link_not_exists.html',
             {'menu': menu}, context_instance=RequestContext(request))
     if isinstance(link, AbsoluteLink):
-        return HttpResponseRedirect(link.get_absolute_url())
+        url_redirect = link.get_absolute_url()
+        if  url_redirect != request.get_full_path():
+            return HttpResponseRedirect(url_redirect)
+        raise Http404
     else:
         context = {}
         if section_slug:
