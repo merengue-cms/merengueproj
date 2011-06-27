7ae0fb90b5e9a6696a6d40248dd943138bf38aef
Pablo Martín <goinnn@gmail.com>
2011-06-27 17:04:24 +0000

    See #2139 Fixes when this content is into a section
    git-svn-id: https://svn.merengueproject.org/trunk@5122 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/section/admin.py
+++ merengueproj/merengue/section/admin.py
@@ -172,6 +172,12 @@ class SectionContentAdmin(OrderableRelatedModelAdmin):
             del form.base_fields['section']
         return form
 
+    def get_accesible_states(self, status, user, obj):
+        if obj or not self.ACQUIRE_SECTION_ROLES:
+            return super(SectionContentAdmin, self).get_accesible_states(status, user, obj)
+        else:
+            return status.get_accesible_states(user, self.basecontent)
+
     def custom_relate_content(self, request, obj, form, change):
         if not change:
             SectionRelatedContent.objects.create(
