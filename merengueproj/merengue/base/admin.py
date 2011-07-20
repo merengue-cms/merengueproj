bf34bd80d6b6d3f6f3e84b77312776315aadc6e1
Pablo Martín <goinnn@gmail.com>
2011-07-20 09:13:47 +0000

    See #2188
    git-svn-id: https://svn.merengueproject.org/trunk@5251 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/base/admin.py
+++ merengueproj/merengue/base/admin.py
@@ -827,7 +827,7 @@ class BaseContentAdmin(BaseOrderableAdmin, WorkflowBatchActionProvider, Permissi
     inherit from BaseContent
     """
     change_list_template = "admin/basecontent/sortable_change_list.html"
-    list_display = ('__unicode__', '', 'user_modification_date', 'last_editor')
+    list_display = ('__unicode__', 'workflow_status', 'user_modification_date', 'last_editor')
     list_display_for_select = ('name', 'status', 'user_modification_date', 'last_editor')
     search_fields = (get_fallback_fieldname('name'), )
     date_hierarchy = 'creation_date'
