875db1557e8292d8ebe2e1a7b361af42075505d5
msaelices <msaelices@yaco.es>
2011-11-09 02:08:47 +0000

    See #2184. Merged revisions 5422 via svnmerge from https://svn.merengueproject.org/trunk
    ........
      r5422 | msaelices | 2011-11-09 03:05:56 +0100 (mié, 09 nov 2011) | 3 lines
      
      Definitively removed DB restore in Merengue, because is impossible do
      a DB restore without disconnecting current connection. Fixes #692.
    ........
    
    
    git-svn-id: https://svn.merengueproject.org/branches/0.8.X@5424 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/base/adminforms.py
+++ merengueproj/merengue/base/adminforms.py
@@ -21,8 +21,6 @@ from django.utils.translation import ugettext_lazy as _
 
 from merengue.base.forms import BaseForm
 
-from cmsutils.db_utils import set_backup
-
 
 class BaseConfigForm(BaseForm):
     zipfile = forms.FileField(label=_("Select a .zip file:"))
@@ -39,12 +37,3 @@ class BaseConfigForm(BaseForm):
 
 class UploadConfigForm(BaseConfigForm):
     pass
-
-
-class BackupForm(BaseConfigForm):
-
-    def save(self):
-        zip_config = self.cleaned_data['zipfile']
-        f = zip_config.infolist()[0]
-        value = zip_config.read(f.filename)
-        set_backup(value)
