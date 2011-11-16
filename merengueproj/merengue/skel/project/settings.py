50b78f69f76577b3048dba44ef6e1b32ed66bfb3
Pablo Martín <goinnn@gmail.com>
2011-11-16 17:02:42 +0000

    See #2265 Remove a error in the settings skel
    git-svn-id: https://svn.merengueproject.org/trunk@5446 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/skel/project/settings.py
+++ merengueproj/merengue/skel/project/settings.py
@@ -102,12 +102,6 @@ LOGIN_REDIRECT_URL = path.join(BASE_URL, LOGIN_REDIRECT_URL[1:])
 LOGOUT_REDIRECT_URL = path.join(BASE_URL, LOGOUT_REDIRECT_URL[1:])
 
 
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/mapa-conocimiento/admin_media/'
-
-
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = ''
 
