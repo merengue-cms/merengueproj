29261a6b01ca3f26d511a40a8bc0e7c1ebab2aa0
jorge correa herrero <jorgecorrea>
2012-02-17 11:23:46 +0000

    see #138 changes to fix translation for model field name for document and event
    git-svn-id: https://svn.merengueproject.org/trunk@5539 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/section/admin.py
+++ merengueproj/merengue/section/admin.py
@@ -214,7 +214,7 @@ class CustomStyleRelatedModelAdmin(RelatedModelAdmin):
 
 
 class DocumentAdmin(BaseContentAdmin):
-    list_display = ('name', 'slug', 'workflow_status', )
+    list_display = ('__unicode__', 'slug', 'workflow_status', )
     html_fields = ('description', 'body', )
     prepopulated_fields = {'slug': (get_fallback_fieldname('name'), )}
     actions = BaseAdmin.actions + ['set_as_published', 'set_as_draft']
