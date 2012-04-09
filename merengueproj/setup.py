948b2853aee5fc214bed43eaf6fdf284e9405ff3
msaelices <msaelices@yaco.es>
2012-04-09 07:12:50 +0000

    Preparing 0.8.2 release. See #2305
    git-svn-id: https://svn.merengueproject.org/branches/0.8.X@5561 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/setup.py
+++ merengueproj/setup.py
@@ -63,7 +63,7 @@ for dirpath, dirnames, filenames in os.walk('.'):
 
 setup(
     name="merengue",
-    version="0.8.1",
+    version="0.8.2",
     install_requires=parse_requirements('requirements.txt'),
     dependency_links=parse_dependency_links('requirements.txt'),
     author="Yaco Sistemas",
