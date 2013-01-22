851555099b4d4ece4e25f07fba8c7153ddc6f13b
Emilio A. Sanchez Lopez <esanchez@cymoril>
2013-01-22 10:28:29 +0000

    Set custommeta plugin as deprecated. See #2365
    git-svn-id: https://svn.merengueproject.org/trunk@5651 5c12d508-d76f-0410-91c7-e3e856e0eb0f

---
+++ merengueproj/plugins/custommeta/__init__.py
@@ -0,0 +1,4 @@
+from warnings import warn
+
+
+warn("Custom meta plugin is deprecated. Please remove it from your active plugins and use the new CustomMeta model in the core plugin", DeprecationWarning)
