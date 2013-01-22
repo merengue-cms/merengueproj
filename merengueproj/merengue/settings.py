ddeaf683e4a34bdb5b70e904c76ad8e79e503c7d
Emilio A. Sanchez Lopez <esanchez@cymoril>
2013-01-22 10:19:23 +0000

    Removed custommeta plugin form required plugins. See #2365
    git-svn-id: https://svn.merengueproject.org/trunk@5650 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/settings.py
+++ merengueproj/merengue/settings.py
@@ -176,7 +176,7 @@ MAX_INT_VALUE = 2147483647
 PLUGINS_DIR = 'plugins'
 
 # merengue required plugins in project (will be activated by default)
-REQUIRED_PLUGINS = ('core', 'custommeta')
+REQUIRED_PLUGINS = ('core', )
 
 # if merengue will detect new plugins in file system
 DETECT_NEW_PLUGINS = False
