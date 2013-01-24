8b653592287a7b1532692bc06939b37bc848e586
msaelices <msaelices@yaco.es>
2013-01-24 17:29:49 +0000

    Preparing 0.9.0 version. See #2367
    git-svn-id: https://svn.merengueproject.org/trunk@5662 5c12d508-d76f-0410-91c7-e3e856e0eb0f

--- merengueproj/merengue/__init__.py
+++ merengueproj/merengue/__init__.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Merengue.  If not, see <http://www.gnu.org/licenses/>.
 
-VERSION = (0, 9, 0, 'alpha', 0)
+VERSION = (0, 9, 0, 'final', 0)
 
 
 def get_version():
